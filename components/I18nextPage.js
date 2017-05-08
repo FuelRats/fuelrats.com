8b318d7285f7ed93f12237f1a53c54379ffd3164
Trezy <tre@trezy.com>
2017-05-08 15:43:11 -0500

    Removing some noise

--- components/I18nextPage.js
+++ components/I18nextPage.js
@@ -19,21 +19,14 @@ export default class extends React.Component {
   \***************************************************************************/
 
   static async getInitialProps () {
-    console.log('Getting initial props...')
     let i18n = await i18next
-    console.log('i18next has initialized')
 
     if (!i18n.isInitialized) {
       await new Promise(resolve => i18n.on('initialized', resolve))
     }
 
     if (!i18n.hasResourceBundle(i18n.language, i18n.options.defaultNS)) {
-      console.log(`i18next is missing the ${i18n.language}/${i18n.options.defaultNS} bundle`)
       await new Promise(resolve => i18n.loadResources(resolve))
-      console.log(`i18next got the ${i18n.language}/${i18n.options.defaultNS} bundle`)
-
-    } else {
-      console.log(`i18next already has the ${i18n.language}/${i18n.options.defaultNS} bundle`)
     }
 
     return {
