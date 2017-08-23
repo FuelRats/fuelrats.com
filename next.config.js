034f12f5e3bf8d65fb288c4f40ad7f5a13096fb6
Trezy <tre@trezy.com>
2017-08-23 14:46:26 -0500

    Webpack bundle analysis
    Adding in Webpack bundle analysis means that we can get a graphical
    representation of the size of the Webpack bundles that we're generating.
    Running the `analyze` script will spin up a server that will stay
    up-to-date as we edit the application.
    
    **Changelog**
    * Add webpack-bundle-analyzer to the custom Webpack config
    * Add an npm script for running the analyzer -- `yarn run analyze`

--- next.config.js
+++ next.config.js
@@ -1,25 +1,18 @@
-//const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-//const { ANALYZE } = process.env
-//
-//module.exports = {
-//  webpack: function (config) {
-//    if (ANALYZE) {
-//      config.plugins.push(new BundleAnalyzerPlugin({
-//        analyzerMode: 'server',
-//        analyzerPort: 8888,
-//        openAnalyzer: true
-//      }))
-//    }
-//
-//    return config
-//  }
-//}
-
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { ANALYZE } = process.env
 const path = require('path')
 const glob = require('glob')
 
 module.exports = {
   webpack: (config, { dev }) => {
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true
+      }))
+    }
+
     config.module.rules.push(
       {
         test: /\.(css|scss)/,
