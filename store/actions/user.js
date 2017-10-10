a24ade345c84714a231f4505595788a0ca231180
Trezy <tre@trezy.com>
2017-10-10 12:54:37 -0500

    [WEB-55] Pull auth actions out of the user action store

--- store/actions/user.js
+++ store/actions/user.js
@@ -50,45 +50,6 @@ export const addNickname = (nickname, password) => async dispatch => {
 
 
 
-export const changePassword = (currentPassword, newPassword) => async dispatch => {
-  dispatch({ type: actionTypes.CHANGE_PASSWORD })
-
-  try {
-    let token = localStorage.getItem('access_token')
-
-    let response = await fetch(`/api/users/setpassword`, {
-      body: JSON.stringify({
-        password: currentPassword,
-        new: newPassword,
-      }),
-      headers: new Headers({
-        Authorization: `Bearer ${token}`,
-      }),
-      method: 'put',
-    })
-
-    response = await response.json()
-
-    dispatch({
-      status: 'success',
-      type: actionTypes.CHANGE_PASSWORD,
-      payload: response,
-    })
-
-  } catch (error) {
-    dispatch({
-      status: 'error',
-      type: actionTypes.CHANGE_PASSWORD,
-    })
-
-    console.log(error)
-  }
-}
-
-
-
-
-
 export const getUser = () => async dispatch => {
   dispatch({ type: actionTypes.GET_USER })
 
@@ -119,81 +80,6 @@ export const getUser = () => async dispatch => {
     dispatch({
       status: 'error',
       type: actionTypes.GET_USER,
-    })
-
-    console.log(error)
-  }
-}
-
-
-
-
-
-export const resetPassword = (password, token) => async dispatch => {
-  dispatch({ type: actionTypes.RESET_PASSWORD })
-
-  try {
-    let response = await fetch(`/api/reset/${token}`, {
-      body: JSON.stringify({
-        password,
-      }),
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      }),
-      method: 'post',
-    })
-
-    response = await response.json()
-
-    dispatch({
-      status: 'success',
-      type: actionTypes.RESET_PASSWORD,
-      payload: response,
-    })
-
-  } catch (error) {
-    dispatch({
-      status: 'error',
-      type: actionTypes.RESET_PASSWORD,
-    })
-
-    console.log(error)
-  }
-}
-
-
-
-
-
-export const sendPasswordResetEmail = email => async dispatch => {
-  dispatch({ type: actionTypes.SEND_PASSWORD_RESET_EMAIL })
-
-  try {
-    let token = localStorage.getItem('access_token')
-
-    let response = await fetch(`/api/reset`, {
-      body: JSON.stringify({
-        email,
-      }),
-      headers: new Headers({
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      }),
-      method: 'post',
-    })
-
-    response = await response.json()
-
-    dispatch({
-      status: 'success',
-      type: actionTypes.SEND_PASSWORD_RESET_EMAIL,
-      payload: response,
-    })
-
-  } catch (error) {
-    dispatch({
-      status: 'error',
-      type: actionTypes.SEND_PASSWORD_RESET_EMAIL,
     })
 
     console.log(error)
