e559a965599706915739aea36db3edffe9bf2e22
Trezy <tre@trezy.com>
2017-12-13 17:30:16 -0600

    Remove the GA tracking ID script

--- scripts/choose-ga-id.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $TRAVIS_BRANCH = 'develop' ]; then
-  export GA_TRACKING_ID=$GA_TRACKING_ID_BETA
-fi
-
-if [ $TRAVIS_BRANCH = 'master' ]; then
-  export GA_TRACKING_ID=$GA_TRACKING_ID_PROD
-fi
