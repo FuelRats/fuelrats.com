00307f9ef2eafc69316bf1a0639052c28183537c
Cameron Welter <camjwelt@gmail.com>
2018-05-14 02:03:03 +0000

    Fix up server file formatting.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+/* eslint-disable strict, global-require */
+
 'use strict'
 
 let isDev = process.env.NODE_ENV !== 'production'
@@ -27,29 +29,29 @@ const next = require('next')({
 \******************************************************************************/
 
 next.prepare()
-.then(() => {
-  // Set up the loggers
-  if (isDev) {
-    require('./config/file-logger')(koa)
-  }
+  .then(() => {
+    // Set up the loggers
+    if (isDev) {
+      require('./config/file-logger')(koa)
+    }
 
-  koa.use(require('koa-no-trailing-slash')())
+    koa.use(require('koa-no-trailing-slash')())
 
-  koa.use(require('koa-logger')())
+    koa.use(require('koa-logger')())
 
-  // Configure proxies
-  require('./config/proxy')(koa, config)
+    // Configure proxies
+    require('./config/proxy')(koa, config)
 
-  // Compress responses
-  koa.use(require('koa-compress')())
+    // Compress responses
+    koa.use(require('koa-compress')())
 
-  // Parse request bodies
-  koa.use(require('koa-body')())
+    // Parse request bodies
+    koa.use(require('koa-body')())
 
-  // Configure the router
-  require('./config/router')(next, koa, config)
+    // Configure the router
+    require('./config/router')(next, koa, config)
 
-  // Start the server
-//  console.log('Listening on port', process.env.PORT || 3000)
-  koa.listen(process.env.PORT || 3000)
-})
+    // Start the server
+    //  console.log('Listening on port', process.env.PORT || 3000)
+    koa.listen(process.env.PORT || 3000)
+  })
