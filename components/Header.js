2b99cf1725247a70a15e5c164aa29e458f3107f1
Cameron Welter <camjwelt@gmail.com>
2018-05-15 20:07:10 +0000

    Add social actions and development info.

--- components/Header.js
+++ components/Header.js
@@ -13,12 +13,16 @@ import BrandSvg from './svg/BrandSvg'
 import Component from './Component'
 
 
+
+// Component constants
+const isDevOrStaging = preval`module.exports = process.env.NODE_ENV !== 'production' || ['develop', 'beta'].includes(process.env.TRAVIS_BRANCH)`
+const buildCommit = preval`module.exports = (process.env.TRAVIS_COMMIT && process.env.TRAVIS_COMMIT.slice(0, 10)) || process.env.TRAVIS_BRANCH || 'Development'`
+const buildCommitRange = preval`module.exports = process.env.TRAVIS_COMMIT_RANGE`
 const hideIconStyle = {
   maxHeight: '0',
   maxWidth: '0',
   opacity: '0',
 }
-
 const showIconStyle = {
   opacity: '1',
 }
@@ -54,7 +58,7 @@ class Header extends Component {
         <input id="nav-control" type="checkbox" />
 
         <label title="Expand/Collapse Menu" htmlFor="nav-control" className="burger button secondary" id="burger">
-          <FontAwesomeIcon icon="bars" />
+          <FontAwesomeIcon style={renderIcons ? showIconStyle : hideIconStyle} icon="bars" />
         </label>
 
         <header role="banner">
@@ -91,9 +95,84 @@ class Header extends Component {
                 </a>
               </Link>
             </li>
+
+            {isDevOrStaging && (
+              <li>
+                <a
+                  className="button link"
+                  href={`https://www.github.com/fuelrats/fuelrats.com/${buildCommitRange ? `compare/${buildCommitRange}` : ''}`}
+                  target="_blank"
+                  rel="noopener noreferrer" >
+                  <FontAwesomeIcon style={renderIcons ? showIconStyle : hideIconStyle} icon="code-branch" listItem />
+                  <span className="link-text">{buildCommit}</span>
+                </a>
+              </li>
+            )}
+
           </ul>
 
-          <div className="social-actions" />
+          <div className="social-actions">
+            <a
+              className="button link icon"
+              href="https://www.twitter.com/FuelRats/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Fuel Rats on Twitter" >
+              <FontAwesomeIcon
+                style={renderIcons ? showIconStyle : hideIconStyle}
+                icon={['fab', 'twitter']}
+                fixedWidth />
+            </a>
+
+            <a
+              className="button link icon"
+              href="https://www.reddit.com/r/FuelRats/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Fuel Rats on Reddit" >
+              <FontAwesomeIcon
+                style={renderIcons ? showIconStyle : hideIconStyle}
+                icon={['fab', 'reddit-alien']}
+                fixedWidth />
+            </a>
+
+            <a
+              className="button link icon"
+              href="https://www.twitch.tv/fuelrats/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Fuel Rats on Twitch" >
+              <FontAwesomeIcon
+                style={renderIcons ? showIconStyle : hideIconStyle}
+                icon={['fab', 'twitch']}
+                fixedWidth />
+            </a>
+
+            <a
+              className="button link icon"
+              href="https://www.github.com/FuelRats/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Tech Rats on GitHub" >
+              <FontAwesomeIcon
+                style={renderIcons ? showIconStyle : hideIconStyle}
+                icon={['fab', 'github']}
+                fixedWidth />
+            </a>
+
+            <a
+              className="button link icon"
+              href="https://forums.frontier.co.uk/showthread.php/150703-Out-of-Fuel-Explorer-Rescue-Service-The-Fuel-Rats"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Fuel Rats on Frontier Forums" >
+              <FontAwesomeIcon
+                style={renderIcons ? showIconStyle : hideIconStyle}
+                icon="space-shuttle"
+                transform={{ rotate: -45 }}
+                fixedWidth />
+            </a>
+          </div>
 
           <div className="join-actions">
             {!loggedIn && !loggingIn && (
