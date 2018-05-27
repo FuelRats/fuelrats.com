9570ea3f1acc740b4e162831b932dd20c9722518
Cameron Welter <camjwelt@gmail.com>
2018-05-27 17:17:22 +0000

    refactor(Page): rewrite page again to serve as a page content wrapper
    We can do away with the page HOC entirely
    by defining authenticationRequired in the component itself.
    react-redux will hoist the static value.
    
    Title is no longer needed since PageWrapper handles the title and header

--- components/Page.js
+++ components/Page.js
@@ -1,33 +1,78 @@
+/* eslint-disable react/no-multi-comp */
+
+// Module imports
+import { Fragment } from 'react'
+import NextHead from 'next/head'
+import PropTypes from 'prop-types'
+
+
+
+
+
 // Component imports
-import { reduxConnect } from '../helpers'
 import Component from './Component'
 
 
 
 
 
-const pageWrapper = (title = 'Page', authReq = false) => Page => (
-  class PageWrapper extends Component {
-    static displayName = `Page(${Page.displayName || Page.name || title.replace(/\s/g, '')})`
-    static title = title
-    static authenticationRequired = Boolean(authReq)
-    static getInitialProps = ctx => (Page.getInitialProps ? Page.getInitialProps(ctx) : {})
+class Page extends Component {
+  constructor (props) {
+    super(props)
 
-    render () {
-      return (<Page {...this.props} />)
+    /* eslint-disable no-console */
+    if (this.props.title.length > 50) {
+      console.warn(`Page titles should be fewer than 60 characters, preferably closer to 50. This page's title is ${this.props.title.length} characters.`)
     }
-  }
-)
 
-const connectPage = (title, authReq, ...connectArgs) => Page => pageWrapper(title, authReq)(reduxConnect(...connectArgs)(Page))
+    if (this.props.description.length > 300) {
+      console.error(`Page description is too long! The description should be 50-300 characters long, but this page's description is ${this.props.description.length} characters.`)
+    }
 
-export default function (title, authReq, ...rest) {
-  if (rest.length) {
-    return connectPage(title, authReq, ...rest)
+    if (this.props.description.indexOf('"') !== -1) {
+      console.error('Page descriptions shouldn\'t contain double quotes.')
+    }
+    /* eslint-enable no-console */
   }
-  return pageWrapper(title, authReq)
+
+  render () {
+    const {
+      children,
+      description,
+      title,
+      renderTitle,
+    } = this.props
+
+    const displayTitle = typeof renderTitle === 'function' ? renderTitle() : (<h1>{title}</h1>)
+    const mainClasses = ['fade-in', 'page', title.toLowerCase().replace(/\s/g, '-')].join(' ')
+
+    return (
+      <Fragment>
+        <NextHead>
+          <title>{title} | The Fuel Rats</title>
+          <meta property="og:title" content={title} />
+          <meta name="description" content={description} />
+          <meta property="og:description" content={description} />
+        </NextHead>
+        <main className={mainClasses}>
+          <header className="page-header">
+            {displayTitle}
+          </header>
+          {children}
+        </main>
+      </Fragment>
+    )
+  }
+
+  static defaultProps = {
+    description: 'The Fuel Rats are Elite: Dangerous\'s premier emergency refueling service. Fueling the galaxy, one ship at a time, since 3301.',
+    renderTitle: null,
+  }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    renderTitle: PropTypes.function,
+    description: PropTypes.string,
+  }
 }
-export {
-  connectPage,
-  pageWrapper,
-}
+
+export default Page
--- pages/_app-layout.js
+++ pages/_app-layout.js
@@ -146,7 +146,6 @@ class AppLayout extends React.Component {
       path,
     } = this.props
 
-    const mainClasses = ['fade-in', 'page', Component.title.toLowerCase().replace(/\s/g, '-')].join(' ')
     return (
       <div role="application">
         <Head title={Component.title} />
@@ -157,9 +156,7 @@ class AppLayout extends React.Component {
 
         <UserMenu />
 
-        <main className={mainClasses}>
-          <Component {...pageProps} />
-        </main>
+        <Component {...pageProps} />
 
         {showLoginDialog && (
           <LoginDialog onClose={() => actions.setFlag('showLoginDialog', false)(store.dispatch)} />
