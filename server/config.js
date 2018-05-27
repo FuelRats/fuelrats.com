e09299de0d96582f1c363a8ea7e117dadbfd76a7
Cameron Welter <camjwelt@gmail.com>
2018-05-27 16:59:42 +0000

    refactor(helpers): remove helper index and rename some helpers.

--- server/config.js
+++ server/config.js
@@ -8,8 +8,6 @@ module.exports = {
     url: process.env.FRDC_EDSM_API_URL || 'https://www.edsm.net',
   },
   wordpress: {
-    username: process.env.FRDC_WORDPRESS_USERNAME,
-    password: process.env.FRDC_WORDPRESS_PASSWORD,
     url: process.env.FRDC_WORDPRESS_URL || 'https://wordpress.fuelrats.com',
   },
 }
