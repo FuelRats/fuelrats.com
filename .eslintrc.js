c628e61b876103b232dc93060323973bbdaa41ad
Cameron Welter <camjwelt@gmail.com>
2018-10-12 22:20:42 +0000

    chore: update eslint rules to utilize new linter functionality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,35 @@
 module.exports = {
   env: {
-    'browser': true
+    browser: true,
   },
   extends: 'airbnb',
   parser: 'babel-eslint',
   plugins: [
-    'babel'
+    'babel',
   ],
   rules: {
     // eslint-core
     'arrow-parens': ['error', 'as-needed'],
     'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    camelcase: ['error', {
+      properties: 'never',
+      allow: [
+        '^UNSAFE_', // for unsafe React lifecycle methods
+      ],
+    }],
     'comma-dangle': ['error', 'always-multiline'],
     'max-classes-per-file': ['error', 2],
     'max-len': ['off'],
     'new-parens': ['off'],
     'no-async-promise-executor': ['error'],
-    'no-fallthrough': ['error', { 'commentPattern': 'fallsthrough'}],
+    'no-fallthrough': ['error', { commentPattern: 'fallsthrough' }],
     'no-misleading-character-class': ['error'],
     'no-multi-assign': ['off'],
-    'no-multiple-empty-lines': ['error', { 'max': 5 }],
+    'no-multiple-empty-lines': ['error', { max: 5 }],
     'no-plusplus': ['off'],
     'no-param-reassign': ['error', {
-      'props': true,
-      'ignorePropertyModificationsFor': [
+      props: true,
+      ignorePropertyModificationsFor: [
         'acc', // for reduce accumulators
         'accumulator', // for reduce accumulators
         'Component', // for React HOCs
@@ -34,31 +40,41 @@ module.exports = {
         'res', // for Express responses
         'response', // for Express responses
         'target', // for ESNext decorators
-      ]
+      ],
     }],
     'no-restricted-syntax': ['off'],
     'no-return-assign': ['off'],
     'no-underscore-dangle': ['off'],
     'no-unused-expressions': ['off'],
+    'padding-line-between-statements': [
+      'warn',
+      // Group const and lets together, enforce blank line after each group
+      { blankLine: 'always', prev: ['const', 'let'], next: '*' },
+      { blankLine: 'never', prev: ['const'], next: ['const'] },
+      { blankLine: 'never', prev: ['let'], next: ['let'] },
+
+      // Blank line separates return statement
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
     'prefer-const': ['error', {
-      'destructuring': 'all',
-      'ignoreReadBeforeAssign': true,
+      destructuring: 'all',
+      ignoreReadBeforeAssign: true,
     }],
     'prefer-object-spread': ['error'],
     'prefer-rest-params': ['off'],
-    'quotes': ['off'],
+    quotes: ['off'],
     'require-atomic-updates': ['error'],
-    'semi': ['off'],
+    semi: ['off'],
     'space-before-function-paren': ['error', 'always'],
     'spaced-comment': ['off'],
 
     // eslint-plugin-babel
     'babel/no-unused-expressions': ['error', {
-      'allowShortCircuit': false,
-      'allowTernary': false,
-      'allowTaggedTemplates': false,
+      allowShortCircuit: false,
+      allowTernary: false,
+      allowTaggedTemplates: false,
     }],
-    'babel/quotes': ['error', 'single', { 'avoidEscape': true }],
+    'babel/quotes': ['error', 'single', { avoidEscape: true }],
     'babel/semi': ['error', 'never'],
 
     // eslint-plugin-import
