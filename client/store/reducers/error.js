fa46d789c656118ca3dffe120055ed44a856ca3e
Cameron Welter <camjwelt@gmail.com>
2018-10-20 11:44:20 +0000

    feat(error-reducer): add action type ignore list to error logger

--- client/store/reducers/error.js
+++ client/store/reducers/error.js
@@ -1,15 +1,29 @@
 /* globals IS_DEVELOPMENT:false, IS_STAGING:false */
+
+// Component imports
 import initialState from '../initialState'
+import actionTypes from '../actionTypes'
 
 
+
+
+// Component constants
 const isDev = IS_DEVELOPMENT || IS_STAGING
+const ignoredTypes = [
+
+  // This pops up on every 404 page due to how our fallback system works.
+  // It's not generally helpful to log
+  actionTypes.GET_WORDPRESS_PAGE,
+]
+
+
 
 
 export default function errorReducer (state = initialState.error, action) {
   if (action.status && action.status === 'error') {
-    /* eslint-disable no-console */
-    console.error('ACTION ERR:', action)
-    /* eslint-enable no-console */
+    if (!ignoredTypes.includes(action.type)) {
+      console.error('ACTION ERR:', action)
+    }
 
     if (isDev) {
       return {
