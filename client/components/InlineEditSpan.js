7e9d791e1b4f964150556964a0bc0dc77b5ff178
Cameron Welter <camjwelt@gmail.com>
2019-03-30 05:12:08 +0000

    feat(InlineEditSpan): add inline text editor component

---
+++ client/components/InlineEditSpan.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+
+
+const InlineEditor = ({
+  canEdit,
+  className,
+  inputClassName,
+  value,
+  ...inputProps
+}) => (
+  canEdit
+    ? (<input className={`inline-editor inline ${inputClassName || ''}`} type="text" value={value} {...inputProps} />)
+    : (<span className={`inline-editor ${className}`}>{value}</span>)
+)
+
+
+
+
+
+export default InlineEditor
