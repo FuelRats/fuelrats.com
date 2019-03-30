a3ad47c5316da6e622555fdfd9a5f3dace29a626
Cameron Welter <camjwelt@gmail.com>
2019-03-30 05:48:30 +0000

    refactor(USerRatsPanel): use rat cards to display rat data
    Simplify component to function component

--- client/components/UserRatsPanel.js
+++ client/components/UserRatsPanel.js
@@ -1,173 +1,42 @@
 // Module imports
-import moment from 'moment'
 import React from 'react'
-import ReactTable from 'react-table'
-
 
 
 
 
 // Component imports
 import { connect } from '../store'
-import { Link } from '../routes'
+import { selectUser } from '../store/selectors'
+import RatCard from './RatCard'
 import AddRatForm from './AddRatForm'
-import Component from './Component'
-import RatDetails from './RatDetails'
 
 
 
+const UserRatsPanel = ({ user }) => {
+  const { rats } = user.relationships
 
-@connect
-class UserRatsPanel extends Component {
-  /***************************************************************************\
-    Private Methods
-  \***************************************************************************/
-
-  static _renderCreatedAtColumn (row) {
-    return moment(row.original.attributes.createdAt).format('DD MMMM, YYYY')
-  }
-
-  static _renderPlatformColumn (row) {
-    return (
-      <div className={['badge', 'platform', 'short', row.original.attributes.platform].join(' ')} />
-    )
-  }
-
-  static _renderNameColumn (row) {
-    const { id } = row.original
-    const { name } = row.original.attributes
-
-    return (
-      <Link route="rats view" params={{ id }}>
-        <a>{name}</a>
-      </Link>
-    )
-  }
-
-  _renderSubcomponent (row) {
-    const ships = row.original.relationships.ships.data.map(({ id }) => this.props.ships.ships.find((ship) => ship.id === id))
-
-    return (
-      <RatDetails ships={ships} />
-    )
-  }
-
-
-
-
-
-  /***************************************************************************\
-    Public Methods
-  \***************************************************************************/
-
-  constructor (props) {
-    super(props)
-
-    this._bindMethods(['onSubmit', '_renderSubcomponent'])
-
-    this.state = {
-      name: '',
-      platform: 'pc',
-    }
-  }
-
-  async onSubmit (event) {
-    const {
-      name,
-      platform,
-    } = this.state
-
-    event.preventDefault()
-
-    const rat = {
-      name,
-      platform,
-    }
-
-    await this.props.createRat(rat)
-  }
-
-  render () {
-    const { rats } = this.props
-
-    return (
-      <div className="panel">
-        <ReactTable
-          className="user-rats"
-          columns={this.columns}
-          data={rats}
-          defaultPageSize={rats.length}
-          manual
-          showPagination={false}
-          SubComponent={this._renderSubcomponent} />
+  return (
+    <div className="user-rats-tab flex column">
+      <div className="flex justify-end">
         <AddRatForm />
       </div>
-    )
-  }
-
-
-
-
-
-  /***************************************************************************\
-    Getters
-  \***************************************************************************/
-
-  get columns () {
-    return [
-      {
-        accessor: 'attributes.platform',
-        Cell: this._renderPlatformColumn,
-        className: 'platform',
-        Header: 'Platform',
-        headerClassName: 'platform',
-        id: 'platform',
-        resizable: true,
-        sortable: true,
-        width: 80,
-      },
-      {
-        accessor: 'attributes.name',
-        Cell: this._renderNameColumn,
-        className: 'name',
-        Header: 'Rat',
-        headerClassName: 'name',
-        id: 'name',
-        resizable: true,
-        sortable: true,
-      },
-      {
-        accessor: 'attributes.createdAt',
-        Cell: this._renderCreatedAtColumn,
-        className: 'createdAt',
-        Header: 'Created At',
-        headerClassName: 'createdAt',
-        id: 'createdAt',
-        resizable: true,
-        sortable: true,
-        width: 150,
-      },
-    ]
-  }
-
-
-
-
-
-  /***************************************************************************\
-    Redux Properties
-  \***************************************************************************/
-
-  static mapDispatchToProps = ['createRat']
-
-  static mapStateToProps = ({ rats, ships, user }) => ({
-    rats: Object.values(rats.rats).filter((rat) => user.id === rat.attributes.userId),
-    ships,
-  })
+      <div className="flex">
+        {rats.data.map(({ id }) => <RatCard key={id} ratId={id} />)}
+      </div>
+    </div>
+  )
 }
 
 
 
 
 
-export default UserRatsPanel
+UserRatsPanel.mapStateToProps = (state) => ({
+  user: selectUser(state),
+})
+
+
+
+
+
+export default connect(UserRatsPanel)
