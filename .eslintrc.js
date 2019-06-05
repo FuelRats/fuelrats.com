beeb9f441521de2fee2512d08256a6a3d35ea44c
Cameron Welter <camjwelt@gmail.com>
2019-06-05 19:54:02 +0000

    refactor(eslintrc): add worker files to import ignore list

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,4 +7,9 @@ module.exports = {
     'react/prop-types': ['off'], // We're not quite ready to enforce prop-types for all files yet
     'jsx-a11y/no-noninteractive-element-interactions': ['off'], // We intend to enable this once we refactor certain key components
   },
+  settings: {
+    'import/ignore': [
+      '\.worker\.js$'
+    ]
+  }
 }
