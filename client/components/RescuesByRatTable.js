bc1bb7ae63da1aca368d5f9767d32d56e95d844b
Cameron Welter <camjwelt@gmail.com>
2019-06-16 16:36:06 +0000

    refactor(RescuesByRatTable): localize loading state, add rat achievement icons, enable pagination and filtering

--- client/components/RescuesByRatTable.js
+++ client/components/RescuesByRatTable.js
@@ -10,9 +10,22 @@ import ReactTable from 'react-table'
 import { connect } from '../store'
 import {
   selectLeaderboard,
-  selectLeaderboardLoading,
 } from '../store/selectors'
+import CodeRedIcon from './Leaderboard/CodeRedIcon'
 import Component from './Component'
+import FirstYearIcon from './Leaderboard/FirstYearIcon'
+import RescueAchievementIcon from './Leaderboard/RescueAchievementIcon'
+import safeParseInt from '../helpers/safeParseInt'
+
+
+
+
+
+const caseInsensitiveFilter = (filter, row) => {
+  const id = filter.pivotId || filter.id
+  return row[id] === undefined ? true : String(row[id]).toLowerCase().startsWith(filter.value.toLowerCase())
+}
+
 
 
 
@@ -20,28 +33,50 @@ import Component from './Component'
 @connect
 class RescuesByRatTable extends Component {
   /***************************************************************************\
+    Class Properties
+  \***************************************************************************/
+
+  state = {
+    loading: true,
+  }
+
+
+
+
+
+  /***************************************************************************\
     Public Methods
   \***************************************************************************/
 
-  componentDidMount () {
-    this.props.getRatLeaderboard()
+  async componentDidMount () {
+    if (!this.props.statistics.length) {
+      await this.props.getRatLeaderboard()
+    }
+    this.setState({ loading: false })
   }
 
   render () {
     const {
-      loading,
       statistics,
     } = this.props
+
+    const {
+      loading,
+    } = this.state
 
     return (
       <section className="panel">
         <ReactTable
-          className="rescues-by-rat"
+          className="rescues-by-rat -striped"
           columns={this.columns}
           data={statistics}
+          defaultPageSize={20}
+          filterable
+          sortable={false}
+          resizable={false}
+          defaultFilterMethod={caseInsensitiveFilter}
           loading={loading}
-          manual
-          showPagination={false} />
+          showPageSizeOptions={false} />
       </section>
     )
   }
@@ -61,20 +96,41 @@ class RescuesByRatTable extends Component {
           accessor: (datum) => datum.attributes['user.displayRat.name'] || datum.attributes.rats[0],
           className: 'name',
           Header: 'Name',
-          headerClassName: 'name',
+          headerClassName: 'name-header',
           id: 'name',
-          resizable: false,
-          sortable: false,
         },
         {
           accessor: 'attributes.rescueCount',
           className: 'rescues',
           Header: 'Rescues',
-          headerClassName: 'rescues',
+          headerClassName: 'rescues-header',
           id: 'rescues',
-          resizable: false,
-          sortable: false,
-          width: 100,
+          filterable: false,
+          width: 150,
+        },
+        {
+          accessor: (datum) => datum,
+          className: 'badges',
+          Header: 'Badges',
+          headerClassName: 'badges-header',
+          id: 'badges',
+          filterable: false,
+          width: 200,
+          Cell: ({ value }) => {
+            const {
+              codeRed,
+              createdAt,
+              rescueCount,
+            } = value.attributes
+
+            return (
+              <span className="svg-list">
+                <RescueAchievementIcon rescueCount={rescueCount} className="size-32 fixed" />
+                <CodeRedIcon codeRedCount={safeParseInt(codeRed)} className="size-32 fixed" />
+                <FirstYearIcon createdAt={createdAt} className="size-32 fixed" />
+              </span>
+            )
+          },
         },
       ]
     }
@@ -85,7 +141,6 @@ class RescuesByRatTable extends Component {
   static mapDispatchToProps = ['getRatLeaderboard']
 
   static mapStateToProps = (state) => ({
-    loading: selectLeaderboardLoading(state),
     statistics: selectLeaderboard(state),
   })
 }
