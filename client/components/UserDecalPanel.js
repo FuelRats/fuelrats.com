3f7a388c04426243c30e0404e91c1c9411781f78
314numberpi <15672322+314numberpi@users.noreply.github.com>
2019-06-21 18:35:32 +0100

    refactor(UserDecalPanel): add decal toggle button[WIP]

--- client/components/UserDecalPanel.js
+++ client/components/UserDecalPanel.js
@@ -1,7 +1,9 @@
 // Module imports
-import moment from 'moment'
+// import moment from 'moment'
 import React from 'react'
-import ReactTable from 'react-table'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { formatAsEliteDate } from '../helpers/formatTime'
+// import ReactTable from 'react-table'
 
 
 
@@ -16,7 +18,7 @@ import Component from './Component'
 
 
 // Component constants
-const ELITE_GAME_YEAR_DESPARITY = 1286 // Years between IRL year and Elite universe year
+// const ELITE_GAME_YEAR_DESPARITY = 1286 // Years between IRL year and Elite universe year
 
 
 
@@ -54,11 +56,12 @@ class UserDetailsPanel extends Component {
     })
   }
 
-  static _renderClaimedAtRow (row) {
-    const rescue = row.original
-
-    return moment(rescue.claimedAt).add(ELITE_GAME_YEAR_DESPARITY, 'years').format('DD MMM, YYYY')
-  }
+  // static _renderClaimedAtRow (row) {
+  //   const rescue = row.original
+  //   // console.log(rescue)
+  //
+  //   return moment(rescue.claimedAt).add(ELITE_GAME_YEAR_DESPARITY, 'years').format('DD MMM, YYYY')
+  // }
 
   _renderNoDataText () {
     const {
@@ -73,7 +76,7 @@ class UserDetailsPanel extends Component {
       }
 
       return (
-        <div>
+        <div className="redeem-decal">
           <p>You're eligible for a decal but you haven't redeemed it yet.</p>
           <button
             onClick={() => this._redeemDecal()}
@@ -85,6 +88,37 @@ class UserDetailsPanel extends Component {
     }
 
     return null
+  }
+
+  _renderDecalCode = (decal, index) => (
+    <>
+      <div className="decal-code" key={`code${index}`}>
+        <button
+          key={`toggle${index}`}
+          aria-label="toggle-decal"
+          className="icon decal-toggle"
+          onClick={this._handleToggle}
+          // title={maxNicksReached ? 'You\'ve used all your nicknames' : 'Add new nickname'}
+          type="button">
+          <FontAwesomeIcon icon={this.state.formOpen[index] ? 'eye-slash' : 'eye'} fixedWidth />
+        </button>
+        {decal.attributes.code}
+      </div>
+      <div className="decal-claimed-at" key={`claimedAt${index}`}>{formatAsEliteDate(decal.attributes.claimedAt)}</div>
+    </>
+  )
+
+  _renderDecalCodes = () => {
+    const { decals } = this.props
+    console.log(decals)
+
+    if (decals === []) {
+      return null
+    }
+
+    return (
+      decals.map(this._renderDecalCode)
+    )
   }
 
 
@@ -108,26 +142,47 @@ class UserDetailsPanel extends Component {
     this.setState({ checkingEligibility: false })
   }
 
+  state = {
+    formOpen: false,
+  }
+
+  _handleToggle = () => {
+    this.setState((state) => ({
+      ...state,
+      formOpen: !state.formOpen,
+    }))
+  }
+
   render () {
     const { decals } = this.props
+    console.log(this)
 
     const {
       checkingEligibility,
       redeeming,
     } = this.state
 
+    const loadingText = checkingEligibility ? 'Checking decal eligibility...' : 'Redeeming decal codes...'
+    // console.log(loadingText)
     return (
-      <ReactTable
-        className="panel user-decals"
-        columns={this.columns}
-        data={decals}
-        defaultPageSize={10}
-        loading={checkingEligibility || redeeming}
-        loadingText={checkingEligibility ? 'Checking decal eligibility...' : 'Redeeming decal codes...'}
-        manual
-        minRows={5}
-        noDataText={this._renderNoDataText()}
-        showPagination={false} />
+      // <ReactTable
+      //   className="panel user-decals"
+      //   columns={this.columns}
+      //   data={decals}
+      //   defaultPageSize={10}
+      //   loading={checkingEligibility || redeeming}
+      //   loadingText={checkingEligibility ? 'Checking decal eligibility...' : 'Redeeming decal codes...'}
+      //   manual
+      //   minRows={3}
+      //   noDataText={this._renderNoDataText()}
+      //   showPagination={false} />
+      <div className="panel user-decals">
+        <header>Decal</header>
+        <div className="panel-content">
+          {(checkingEligibility || redeeming) ? (<div className="loading">{loadingText}</div>) : (this._renderDecalCodes(decals))}
+          {(decals === []) ? (this._renderNoDataText()) : ('')}
+        </div>
+      </div>
     )
   }
 
@@ -139,30 +194,30 @@ class UserDetailsPanel extends Component {
     Getters
   \***************************************************************************/
 
-  get columns () {
-    return [
-      {
-        accessor: 'attributes.code',
-        className: 'code',
-        Header: 'Decal Code',
-        headerClassName: 'code',
-        id: 'code',
-        resizable: false,
-        sortable: false,
-      },
-      {
-        accessor: 'attributes.claimedAt',
-        Cell: this._renderClaimedAtRow,
-        className: 'claimedAt',
-        Header: 'Redeemed',
-        headerClassName: 'claimedAt',
-        id: 'claimedAt',
-        resizable: false,
-        sortable: true,
-        width: 120,
-      },
-    ]
-  }
+  // get columns () {
+  //   return [
+  //     {
+  //       accessor: 'attributes.code',
+  //       className: 'code',
+  //       Header: 'Decal Code',
+  //       headerClassName: 'code',
+  //       id: 'code',
+  //       resizable: false,
+  //       sortable: false,
+  //     },
+  //     {
+  //       accessor: 'attributes.claimedAt',
+  //       Cell: this._renderClaimedAtRow,
+  //       className: 'claimedAt',
+  //       Header: 'Redeemed',
+  //       headerClassName: 'claimedAt',
+  //       id: 'claimedAt',
+  //       resizable: false,
+  //       sortable: true,
+  //       width: 120,
+  //     },
+  //   ]
+  // }
 
 
 
