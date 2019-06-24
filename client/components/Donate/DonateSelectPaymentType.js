46f2a3d6e4bb9885080a25d97bf305a5fb039bc7
Chris Gårdenberg <webmaster@itssimple.se>
2019-06-24 23:59:11 +0200

    refactor(DonateSelectPaymentType): Added controls and events

--- client/components/Donate/DonateSelectPaymentType.js
+++ client/components/Donate/DonateSelectPaymentType.js
@@ -1,14 +1,73 @@
 import React from 'react'
+import ValidatedCurrencySelect from '../ValidatedCurrencySelect'
 
+
+const INVALID_CURRENCY_MESSAGE = 'Currency is Required'
+const initialState = {
+  currency: '',
+  validity: {
+    currency: INVALID_CURRENCY_MESSAGE,
+  },
+}
 
 class DonateSelectPaymentType extends React.Component {
+  state = {
+    ...initialState,
+  }
+
   static noop () {
     return null
   }
 
+  _handleFieldChange = ({ target, valid, message }) => {
+    this.setState(({ validity }) => {
+      const {
+        name,
+        value,
+      } = target
+      const required = typeof validity[name] !== 'undefined'
+
+      return {
+        [name]: value,
+        ...(required
+          ? {
+            validity: {
+              ...validity,
+              [name]: valid || message,
+            },
+          }
+          : {}),
+      }
+    })
+  }
+
   render () {
+    const { currency } = this.state
     return (
-      <div>Select payment type</div>
+      <div className="">
+        <h5>I want to donate some</h5>
+        <div>
+          <ValidatedCurrencySelect
+            className="currency-input"
+            disabled={this.props.submitting}
+            id="donateCurrency"
+            invalidMessage={INVALID_CURRENCY_MESSAGE}
+            name="currency"
+            label="Currency"
+            onChange={this._handleFieldChange}
+            required
+            value={currency} />
+        </div>
+        <h5>with my</h5>
+        <div>
+          <button type="button" onClick={() => this.props.onTypeClick('card', currency)}>
+            Credit card!
+          </button>
+          <button type="button" onClick={() => this.props.onTypeClick('aplgogl', currency)}>
+            Apple / Google Pay!
+          </button>
+        </div>
+      </div>
     )
   }
 }
