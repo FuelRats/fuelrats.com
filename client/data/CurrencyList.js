d88938de4a0072c4c390b7166fbb981ec99bba8d
Chris Gårdenberg <webmaster@itssimple.se>
2019-06-24 23:30:00 +0200

    feat(ValidatedCurrencySelect): Added currency selector

---
+++ client/data/CurrencyList.js
@@ -0,0 +1,7 @@
+const currencyList = [
+  { CurrencyCode: 'EUR', Text: 'Euro €' },
+  { CurrencyCode: 'EUR', Text: 'US Dollar $' },
+  { CurrencyCode: 'EUR', Text: 'British Pound £' },
+]
+
+export default currencyList
