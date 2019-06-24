4ad47a27613019ee5a74f2f4520bfdd40d6130a1
Chris Gårdenberg <webmaster@itssimple.se>
2019-06-25 00:00:53 +0200

    refactor(donate): Added text and basic functionality

--- client/pages/donate.js
+++ client/pages/donate.js
@@ -9,21 +9,52 @@ import DonateSelectPaymentType from '../components/Donate/DonateSelectPaymentTyp
 
 class Donate extends React.Component {
     state = {
-      hasSelectedType: false,
+      paymentType: null,
+      currency: '',
+      amount: null,
+      submitting: false,
     }
 
-    static noop () {
-      return null
+    _handleTypeClick = (type, currency) => {
+      this.setState({
+        paymentType: type,
+        currency,
+      })
     }
 
     render () {
-      const { hasSelectedType, paymentType, currency, amount } = this.state
+      const {
+        paymentType, currency, amount, submitting,
+      } = this.state
       return (
-        <PageWrapper title="Donate">
+        <PageWrapper title="Donate" displayTitle="Donate to the Mischief">
           <div className="page-content">
-            {hasSelectedType
-              ? <DonatePaymentType type={paymentType} currency={currency} amount={amount} />
-              : <DonateSelectPaymentType />
+            <p>
+              Thank you for considering donating to The Fuel Rats. As simple as our jobs are in the end,
+              we actually have a lot of systems in place to help us do our rescues,
+              and the servers they run on amount to over €100 per month.
+            </p>
+            <p>
+              We've got rats who have fronted the money to keep these servers running,
+              but if you'd like to contribute, that would be great!
+            </p>
+            <p>
+              Our donations are processed through Stripe, and they take most major credit cards.
+            </p>
+            {paymentType && currency !== ''
+              ? (
+                <DonatePaymentType
+                  type={paymentType}
+                  currency={currency}
+                  amount={amount}
+                  submitting={submitting} />
+              )
+              : (
+                <DonateSelectPaymentType
+                  type={paymentType}
+                  currency={currency}
+                  onTypeClick={this._handleTypeClick} />
+              )
             }
           </div>
         </PageWrapper>
