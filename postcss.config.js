04b2df13723510cbec32da8bc5ae4930866bcc84
Cameron Welter <camjwelt@gmail.com>
2019-06-25 07:16:40 +0000

    chore: add stylelint and associated scripts/configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
 /* eslint-disable global-require */
 module.exports = {
   plugins: [
-    require('postcss-easy-import')({ prefix: '_' }), // keep this first
-    require('autoprefixer')({ /* ...options */ }), // so imports are auto-prefixed too
+    require('postcss-easy-import')({
+      prefix: '_',
+      plugins: ([
+        require('stylelint')({}), // lint before merge.
+      ]),
+    }), // inline @imports
+    require('autoprefixer')({}), // so imports are auto-prefixed too
   ],
 }
