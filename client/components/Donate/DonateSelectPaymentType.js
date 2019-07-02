7523d5ddb0211452d491919d2f3c363f0bf24c82
Chris Gårdenberg <webmaster@itssimple.se>
2019-07-02 08:27:28 +0200

    refactor(DonateSelectPaymentType): Added amount selection

--- client/components/Donate/DonateSelectPaymentType.js
+++ client/components/Donate/DonateSelectPaymentType.js
@@ -1,11 +1,30 @@
 import React from 'react'
-import ValidatedCurrencySelect from '../ValidatedCurrencySelect'
 
+
+
+import ValidatedCurrencySelect from '../ValidatedCurrencySelect'
+import RadioCardInput from '../RadioCardInput'
+
+const VALID_AMOUNTS = [
+  {
+    key: '1.0',
+    value: 1.0,
+  },
+  {
+    key: '5.0',
+    value: 5.0,
+  },
+  {
+    key: '10.0',
+    value: 10.0,
+  },
+]
 
 const INVALID_CURRENCY_MESSAGE = 'Currency is Required'
 
 class DonateSelectPaymentType extends React.Component {
   state = {
+    amount: 1.0,
     currency: '',
     validity: {
       currency: INVALID_CURRENCY_MESSAGE,
@@ -34,11 +53,33 @@ class DonateSelectPaymentType extends React.Component {
     })
   }
 
+  _handleAmountChange = (event) => {
+    this.setState({
+      amount: event.key,
+    })
+  }
+
   render () {
-    const { currency } = this.state
+    const { currency, amount } = this.state
     return (
       <div className="">
-        <h5>I want to donate some</h5>
+        <h5>I want to donate</h5>
+        <div>
+          <RadioCardInput
+            value={amount}
+            onChange={this._handleAmountChange}
+            options={
+            VALID_AMOUNTS
+            }>
+            {({ value }) => (
+              <>
+                <span>
+                  {value}
+                </span>
+              </>
+            )}
+          </RadioCardInput>
+        </div>
         <div>
           <ValidatedCurrencySelect
             className="currency-input"
