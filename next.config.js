deb515f9cfa413b4604180e3ea029e2213a4fa2a
Cameron Welter <camjwelt@gmail.com>
2019-07-05 01:13:56 +0000

    chore: reconfigure next.js config to accept new teamcity environment.

--- next.config.js
+++ next.config.js
@@ -15,15 +15,14 @@ const withWorkers = require('@zeit/next-workers')
 // Component constants
 const {
   ANALYZE,
+  BUILD_VCS_BRANCH,
+  BUILD_VCS_NUMBER,
   FRDC_API_URL,
   FRDC_PUBLIC_URL,
   FRDC_STRIPE_API_PK,
   PORT,
-  TRAVIS,
-  TRAVIS_BRANCH,
-  TRAVIS_BUILD_ID,
-  TRAVIS_COMMIT,
-  TRAVIS_COMMIT_RANGE,
+  TEAMCITY,
+  TEAMCITY_BUILD_URL,
 } = process.env
 
 const DEFAULT_PORT = 3000
@@ -36,8 +35,8 @@ const DEV_BUILD_ID_LENGTH = 16
 
 module.exports = withWorkers(withSass({
   generateBuildId: () => (
-    TRAVIS
-      ? TRAVIS_COMMIT.toLowerCase()
+    TEAMCITY
+      ? BUILD_VCS_NUMBER.toLowerCase()
       : `DEV_${crypto.randomBytes(DEV_BUILD_ID_LENGTH).toString('hex').toLowerCase()}`
   ),
   publicRuntimeConfig: {
@@ -68,13 +67,12 @@ module.exports = withWorkers(withSass({
 
     config.plugins.push(new webpack.DefinePlugin({
       $IS_DEVELOPMENT: JSON.stringify(data.dev),
-      $IS_STAGING: JSON.stringify(['develop', 'beta'].includes(TRAVIS_BRANCH)),
-      $BUILD_BRANCH: JSON.stringify(TRAVIS_BRANCH || 'develop'),
-      $BUILD_COMMIT: JSON.stringify((TRAVIS_COMMIT && TRAVIS_COMMIT.slice(0, COMMIT_HASH_LENGTH)) || TRAVIS_BRANCH || 'develop'),
-      $BUILD_COMMIT_HASH: JSON.stringify(TRAVIS_COMMIT),
-      $BUILD_COMMIT_RANGE: JSON.stringify(TRAVIS_COMMIT_RANGE),
+      $IS_STAGING: JSON.stringify(['develop', 'beta'].includes(BUILD_VCS_BRANCH)),
+      $BUILD_BRANCH: JSON.stringify(BUILD_VCS_BRANCH || 'develop'),
+      $BUILD_COMMIT: JSON.stringify(BUILD_VCS_NUMBER),
+      $BUILD_COMMIT_SHORT: JSON.stringify((BUILD_VCS_NUMBER && BUILD_VCS_NUMBER.slice(0, COMMIT_HASH_LENGTH)) || BUILD_VCS_BRANCH || 'develop'),
       $BUILD_DATE: JSON.stringify((new Date()).toISOString()),
-      $BUILD_ID: JSON.stringify(TRAVIS_BUILD_ID),
+      $BUILD_URL: JSON.stringify(TEAMCITY_BUILD_URL),
       $NEXT_BUILD_ID: JSON.stringify(data.buildId),
       $NODE_VERSION: JSON.stringify(process.version),
     }))
