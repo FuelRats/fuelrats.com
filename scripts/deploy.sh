d015f7bf5a68615c33edcb987e460d2c9117e19c
Cameron Welter <camjwelt@gmail.com>
2019-07-06 06:16:18 +0000

    chore: add updated deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Variables
+
+DEPLOY_DIR="trash"
+SERVICE_NAME=""
+
+# Find deploy target and service name
+case $1 in
+develop)
+  DEPLOY_DIR="dev.fuelrats.com"
+  SERVICE_NAME="fr-web_dev"
+  ;;
+
+
+beta)
+  DEPLOY_DIR="beta.fuelrats.com"
+  SERVICE_NAME="fr-web_beta"
+  ;;
+
+
+master)
+  DEPLOY_DIR="fuelrats.com"
+  SERVICE_NAME="fr-web"
+  ;;
+
+
+*)
+  echo "Current branch is not configured for auto-deploy. skipping deployment..."
+  exit 0
+  ;;
+esac
+
+# Move built project files to server
+rsync -r --delete-after ./dist fuelrats@emmental.fuelrats.com:/var/www/$DEPLOY_DIR/
+
+# restart service
+ssh -t fuelrats@emmental.fuelrats.com "sudo systemctl restart $SERVICE_NAME.service"
