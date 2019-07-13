c25ce8d17c6899caa199e5f44e4917e54d5997f0
Cameron Welter <camjwelt@gmail.com>
2019-07-13 05:06:29 +0000

    refactor(FirstLoginModal): adjust layout, put link button into footer.

--- client/components/FirstLoginModal.js
+++ client/components/FirstLoginModal.js
@@ -8,25 +8,27 @@ import React from 'react'
 // Component imports
 import { connect } from '../store'
 import { selectUserDisplayRat } from '../store/selectors'
-import asModal, { ModalContent } from './Modal'
+import asModal, { ModalContent, ModalFooter } from './Modal'
 
 
 
 
 const FirstLoginModal = () => (
-  <ModalContent className="flex column justify-center align-text">
-    <p>
-      <br />
-      Your name might be on the roster, but there is much more to being a Fuel Rat.
-      <br />
-      <br />
-      The information and guides in the link below will help you along your way. Good luck, and fly safe CMDR!
-      <br />
-      <br />
-    </p>
-    <a className="button" target="_blank" rel="noopener noreferrer" href="https://t.fuelr.at/join">New Rat Documentation</a>
-    <br />
-  </ModalContent>
+  <>
+    <ModalContent className="flex column justify-center align-text">
+      <p>
+        Your name might be on the roster, but there is much more to being a Fuel Rat.
+        <br />
+        <br />
+        The information and guides in the link below will help you along your way. Good luck, and fly safe CMDR!
+      </p>
+    </ModalContent>
+    <ModalFooter>
+      <div className="primary">
+        <a className="button" target="_blank" rel="noopener noreferrer" href="https://t.fuelr.at/join">New Rat Documentation</a>
+      </div>
+    </ModalFooter>
+  </>
 )
 
 FirstLoginModal.mapStateToProps = (state) => ({
@@ -39,7 +41,7 @@ FirstLoginModal.mapStateToProps = (state) => ({
 
 const modalProps = {
   className: 'first-login-dialog',
-  title: 'Welcome to the Fuel Rats!',
+  title: 'Welcome to The Fuel Rats!',
 }
 
 export default asModal(modalProps)(connect(FirstLoginModal))
