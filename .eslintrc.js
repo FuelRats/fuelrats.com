d484392f4bd3f6d01c9fb7db2d202ce6f8fcc6bc
Cameron Welter <camjwelt@gmail.com>
2019-07-24 04:49:30 +0000

    style: allow scss files to be unnamed imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,11 @@ module.exports = {
   rules: {
     'jsx-a11y/no-noninteractive-element-interactions': ['off'], // We intend to enable this once we refactor certain key components
     'react/prop-types': ['off'], // We're not quite ready to enforce prop-types for all files yet
+    'import/no-unassigned-import': ['error', {
+      allow: [
+        '**/*.scss', /* We allow unassigned SCSS imports for css injections */
+      ],
+    }],
   },
   settings: {
     'import/ignore': [
