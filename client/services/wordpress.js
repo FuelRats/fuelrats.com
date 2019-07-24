8b75042db6b73b6adaf459c39e3e218178a20b52
Cameron Welter <camjwelt@gmail.com>
2019-07-24 04:53:37 +0000

    refactor(services): make axios service instances always resolve

--- client/services/wordpress.js
+++ client/services/wordpress.js
@@ -3,22 +3,24 @@ import axios from 'axios'
 import getConfig from 'next/config'
 
 
+
+
+
 const { publicRuntimeConfig } = getConfig()
 const localWordpressApiUrl = publicRuntimeConfig.apis.wordpress.url
 
-let instance = null
+
+
+
+
+const wpService = axios.create({
+  baseURL: localWordpressApiUrl,
+  timeout: 10000,
+  validateStatus: () => true, // Always resolve because it's simpler for the action creators.
+})
 
 
 
 
 
-export default function getWordpressService () {
-  if (!instance) {
-    instance = axios.create({
-      baseURL: localWordpressApiUrl,
-      headers: { 'Content-Type': 'application/json' },
-      timeout: 10000,
-    })
-  }
-  return instance
-}
+export default wpService
