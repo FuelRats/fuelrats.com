fabcba0633d63d4f1b2d6eefee02ae967aee592c
Cameron Welter <camjwelt@gmail.com>
2019-12-28 15:32:10 +0000

    feat(server): add stripe secret key for server

--- server/environment.js
+++ server/environment.js
@@ -22,4 +22,7 @@ module.exports = {
   wordpress: {
     url: process.env.FRDC_WORDPRESS_URL || 'https://wordpress.fuelrats.com',
   },
+  stripe: {
+    secret: process.env.FRDC_STRIPE_API_SK,
+  },
 }
