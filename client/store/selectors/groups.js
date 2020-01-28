89a7bf68dbaf4d67a3ff171904acc2ef24977174
Cameron Welter <camjwelt@gmail.com>
2020-01-28 20:54:27 +0000

    feat(group-selectors): integrate permission checking into store selectors

--- client/store/selectors/groups.js
+++ client/store/selectors/groups.js
@@ -1,6 +1,7 @@
 // Module imports
+import castArray from 'lodash/castArray'
+import intersection from 'lodash/intersection'
 import { createSelector } from 'reselect'
-
 
 
 
@@ -28,6 +29,30 @@ const selectGroupsByUserId = createSelector(
 )
 
 
+/**
+ * To be moved to `users.js` in APIv3 migration.
+ * Profiles in v3 will have an array of scopes we can check from instead of us having to search through groups ourselves.
+ * For v2, this unfortunately needs to be located in `groups.js` to avoid a cyclic dependency.
+ */
+const selectUserByIdHasScope = createSelector(
+  [
+    selectGroupsByUserId,
+    (_state, { scope } = {}) => scope,
+  ],
+  (userGroups, scope) => {
+    if (!userGroups.length || !scope) {
+      return false
+    }
+
+    return userGroups.some(
+      (group) => group.type === 'groups'
+        && intersection(
+          group.attributes?.permissions ?? [],
+          castArray(scope),
+        ).length > 0,
+    )
+  },
+)
 
 
 
@@ -35,4 +60,5 @@ export {
   selectGroupById,
   selectGroups,
   selectGroupsByUserId,
+  selectUserByIdHasScope,
 }
