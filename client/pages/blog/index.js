3acd2cca31d07a14d280ce3cba696b766972af93
Cameron Welter <camjwelt@gmail.com>
2020-01-30 22:31:31 +0000

    refactor(blog-pages): use ArticleCard over internal JSX

--- client/pages/blog/index.js
+++ client/pages/blog/index.js
@@ -1,6 +1,5 @@
 // Module imports
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import moment from 'moment'
+
 import React from 'react'
 
 
@@ -8,14 +7,12 @@ import React from 'react'
 
 // Component imports
 import { PageWrapper } from '../../components/AppLayout'
-import TextPlaceholder from '../../components/TextPlaceholder'
+import ArticleCard from '../../components/Blog/ArticleCard'
 import safeParseInt from '../../helpers/safeParseInt'
 import { Link } from '../../routes'
 import { connect } from '../../store'
 import {
   selectBlogs,
-  selectBlogAuthors,
-  selectBlogCategories,
   selectBlogStatistics,
 } from '../../store/selectors'
 
@@ -61,19 +58,23 @@ class Blogs extends React.Component {
       <menu
         type="toolbar">
         <div className="secondary">
-          {(page > 1) && (
-            <Link route="blog list" params={{ author, category, page: Math.max(1, page - 1) }}>
-              <a className="button">{'Previous Page'}</a>
-            </Link>
-          )}
+          {
+            (page > 1) && (
+              <Link route="blog list" params={{ author, category, page: Math.max(1, page - 1) }}>
+                <a className="button">{'Previous Page'}</a>
+              </Link>
+            )
+          }
         </div>
 
         <div className="primary">
-          {(page < totalPages) && (
-            <Link route="blog list" params={{ author, category, page: Math.min(page + 1, totalPages) }}>
-              <a className="button">{'Next Page'}</a>
-            </Link>
-          )}
+          {
+            (page < totalPages) && (
+              <Link route="blog list" params={{ author, category, page: Math.min(page + 1, totalPages) }}>
+                <a className="button">{'Next Page'}</a>
+              </Link>
+            )
+          }
         </div>
       </menu>
     )
@@ -146,7 +147,7 @@ class Blogs extends React.Component {
   }
 
   render () {
-    const { authors, blogs, categories } = this.props
+    const { blogs } = this.props
     const {
       retrieving,
     } = this.state
@@ -155,78 +156,13 @@ class Blogs extends React.Component {
       <PageWrapper title="Blog">
         <div className="page-content">
           <ol className="article-list loading">
-            {!retrieving && Boolean(blogs.length) && blogs.map((blog) => {
-              const {
-                id,
-              } = blog
-              const postedAt = moment(blog.date_gmt)
-              const author = authors[blog.author] || {
-                id: blog.author,
-                name: (<TextPlaceholder size={30} loading />),
-              }
-
-              /* eslint-disable react/no-danger */
-              return (
-                <li key={id}>
-                  <article>
-                    <header>
-                      <h3 className="title">
-                        <Link route="blog view" params={{ blogId: id }}>
-                          <a dangerouslySetInnerHTML={{ __html: blog.title.rendered }} />
-                        </Link>
-                      </h3>
-                    </header>
-
-                    <small>
-                      <span className="posted-date">
-                        <FontAwesomeIcon icon="clock" fixedWidth />
-                        {'Posted '}
-                        <time dateTime={0}>{postedAt.format('DD MMMM, YYYY')}</time>
-                      </span>
-
-                      <span className="author">
-                        <FontAwesomeIcon icon="user" fixedWidth />
-                        <Link route="blog list" params={{ author: author.id }}>
-                          <a>{author.name}</a>
-                        </Link>
-                      </span>
-
-                      <span>
-                        <FontAwesomeIcon icon="folder" fixedWidth />
-
-                        <ul className="category-list">
-                          {blog.categories.map((catId) => {
-                            const category = categories[catId] || {
-                              id: catId,
-                              description: 'Loading...',
-                              name: (<TextPlaceholder size={25} loading />),
-                            }
-
-                            const {
-                              description,
-                              name,
-                            } = category
-
-                            return (
-                              <li key={category.id}>
-                                <Link route="blog list" params={{ category: category.id }}>
-                                  <a title={description}>{name}</a>
-                                </Link>
-                              </li>
-                            )
-                          })}
-                        </ul>
-                      </span>
-                    </small>
-
-                    <div
-                      className="article-content"
-                      dangerouslySetInnerHTML={{ __html: blog.excerpt.rendered }} />
-                  </article>
+            {
+              !retrieving && Boolean(blogs.length) && blogs.map((blog) => (
+                <li key={blog.id}>
+                  <ArticleCard blogId={blog.id} renderMode="excerpt" />
                 </li>
-              )
-              /* eslint-enable */
-            })}
+              ))
+            }
           </ol>
 
           {this._renderMenu()}
@@ -237,8 +173,6 @@ class Blogs extends React.Component {
 
   static mapStateToProps = (state) => ({
     blogs: selectBlogs(state),
-    authors: selectBlogAuthors(state),
-    categories: selectBlogCategories(state),
     ...selectBlogStatistics(state),
   })
 
