215ecfc0159cf4893713a32d5092c0c591e07e96
Cameron Welter <camjwelt@gmail.com>
2020-02-01 06:38:56 +0000

    chore: migrate to @fuelrats/eslint-config@2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,14 @@ module.exports = {
     browser: true,
     commonjs: true,
   },
-  extends: '@fuelrats/eslint-config-react',
+  extends: [
+    '@fuelrats/eslint-config',
+    '@fuelrats/eslint-config-react',
+  ],
   rules: {
     'jsx-a11y/no-noninteractive-element-interactions': ['off'], // We intend to enable this once we refactor certain key components
     'react/prop-types': ['off'], // We're not quite ready to enforce prop-types for all files yet
+    'jsdoc/require-jsdoc': ['off'], // we'll get to it someday.
   },
   settings: {
     'import/ignore': [
