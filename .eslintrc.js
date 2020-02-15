4712982c7005b578548594f22b9acd95ce4d774f
Cameron Welter <camjwelt@gmail.com>
2020-02-15 18:12:10 +0000

    refactor(eslint-config): adjust local rules to reflect pending rules for our config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,11 @@ module.exports = {
   ],
   rules: {
     'jsx-a11y/no-noninteractive-element-interactions': ['off'], // We intend to enable this once we refactor certain key components.
-    'jsx-a11y/no-onchange': ['off'], // We will enable this when we are finished with refactoring certain components.
-    'react/no-did-mount-set-state': ['off'], // To be fixed with eslint-config-react@2.0.1
     'react/prop-types': ['off'], // We're not quite ready to enforce prop-types for all files yet.
     'jsdoc/require-jsdoc': ['off'], // we'll get to it someday...
+    'jsx-a11y/no-onchange': ['off'], // To be fixed with eslint-config-react@2.1.0
+    'react/no-did-mount-set-state': ['off'], // To be fixed with eslint-config-react@2.1.0
+    'import/no-cycle': ['error'], // To be fixed with eslint-config-react@2.1.0
   },
   settings: {
     'import/ignore': [
