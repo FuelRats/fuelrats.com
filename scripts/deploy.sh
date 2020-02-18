5dfd835fba5df4c7929fe0f1e553d8b831184364
Cameron Welter <camjwelt@gmail.com>
2020-02-18 02:48:00 +0000

    refactor(deploy-script): add new post-rsync commands, remove beta configuration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,18 +12,10 @@ develop)
   SERVICE_NAME="fr-web_dev"
   ;;
 
-
-beta)
-  DEPLOY_DIR="beta.fuelrats.com"
-  SERVICE_NAME="fr-web_beta"
-  ;;
-
-
 master)
   DEPLOY_DIR="fuelrats.com"
   SERVICE_NAME="fr-web"
   ;;
-
 
 *)
   echo "Current branch is not configured for auto-deploy. skipping deployment..."
@@ -32,7 +24,7 @@ master)
 esac
 
 # Move built project files to server
-rsync -r --delete-after ./dist/ fuelrats@emmental.fuelrats.com:/var/www/$DEPLOY_DIR/
+rsync -r ./dist/ fuelrats@emmental.fuelrats.com:/var/www/$DEPLOY_DIR/
 
-# restart service
-ssh -t fuelrats@emmental.fuelrats.com "sudo systemctl restart $SERVICE_NAME.service"
+# Update deps and restart service
+ssh -t fuelrats@emmental.fuelrats.com "cd /var/www/$DEPLOY_DIR/ && yarn install --frozen-lockfile && sudo systemctl restart $SERVICE_NAME.service"
