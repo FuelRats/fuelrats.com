3ec51a4c4b8531cd1e76ef4138e50ea1b1c34a71
Elad Karni <elad.karni@gmail.com>
2020-04-03 00:52:55 -0400

    style(verify): made all exports inline

--- client/store/actions/verify.js
+++ client/store/actions/verify.js
@@ -1,7 +1,7 @@
 import { frApiRequest } from './services'
 import actionTypes from '../actionTypes'
 
-const verifyEmailToken = (token) => frApiRequest(
+export const verifyEmailToken = (token) => frApiRequest(
   actionTypes.VERIFY_EMAIL,
   {
     url: `/verifications/${token}`,
@@ -9,14 +9,12 @@ const verifyEmailToken = (token) => frApiRequest(
   }
 )
 
-const verifySessionToken = (token) => frApiRequest(actionTypes.VERIFY_SESSION, {
+export const verifySessionToken = (token) => frApiRequest(actionTypes.VERIFY_SESSION, {
   url: `/sessions/${token}`,
   method: 'get',
 })
 
-const verifyResetToken = (token) => frApiRequest(actionTypes.VERIFY_RESET, {
+export const verifyResetToken = (token) => frApiRequest(actionTypes.VERIFY_RESET, {
   url: `/resets/${token}`,
   method: 'get',
 })
-
-export { verifyEmailToken, verifySessionToken, verifyResetToken }
