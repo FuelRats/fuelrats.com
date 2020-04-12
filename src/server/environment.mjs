efcc81b1cfbacc74bd25c13a0c4bbb677cf9de6e
Cameron Welter <camjwelt@gmail.com>
2020-04-12 04:12:12 +0000

    feat(server): add proxy setting to environment

--- src/server/environment.mjs
+++ src/server/environment.mjs
@@ -10,7 +10,7 @@ const DEFAULT_PORT = 3000
 const env = {
   isDev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || DEFAULT_PORT,
-  bansFileDir: process.env.FRDC_STRIPE_BANS_FILE,
+  proxyEnabled: process.env.FRDC_PROXIED ?? process.env.NODE_ENV === 'production',
   publicUrl: process.env.FRDC_PUBLIC_URL || `http://localhost:${process.env.PORT || DEFAULT_PORT}`,
   api: {
     clientId: process.env.FRDC_API_KEY,
@@ -25,6 +25,7 @@ const env = {
   },
   stripe: {
     secret: process.env.FRDC_STRIPE_API_SK,
+    bansFile: process.env.FRDC_STRIPE_BANS_FILE,
   },
 }
 
