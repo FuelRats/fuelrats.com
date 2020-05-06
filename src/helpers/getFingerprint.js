070a0ccc278d46baf72a21b079655b62c7f0bd73
Cameron Welter <camjwelt@gmail.com>
2020-05-06 17:39:22 +0000

    refactor(getFingerprint-helper): store fingerprint hash and recall it when requested so we prevent minute changes from invalidating the session

--- src/helpers/getFingerprint.js
+++ src/helpers/getFingerprint.js
@@ -4,6 +4,7 @@ import UAParser from 'ua-parser-js'
 import safeIdleCallback from './safeIdleCallback'
 
 const FINGERPRINT_SEED = 31
+const HASH_KEY = 'fr.ash'
 
 const fingerprintOptions = {
   preprocessor: (key, value) => {
@@ -40,17 +41,25 @@ const fingerprintOptions = {
 
 export default function getFingerprint () {
   return new Promise((resolve, reject) => {
+    let fpHash = localStorage.getItem(HASH_KEY)
+
+    if (fpHash) {
+      resolve(fpHash)
+      return
+    }
+
     try {
       safeIdleCallback(() => {
         fingerprint.get(fingerprintOptions, (components) => {
-          resolve(
-            fingerprint.x64hash128(
-              components.reduce((acc, { value }) => {
-                return acc + value.toString()
-              }, ''),
-              FINGERPRINT_SEED,
-            ),
+          fpHash = fingerprint.x64hash128(
+            components.reduce((acc, { value }) => {
+              return acc + value.toString()
+            }, ''),
+            FINGERPRINT_SEED,
           )
+
+          localStorage.setItem(HASH_KEY, fpHash)
+          resolve(fpHash)
         })
       })
     } catch (error) {
