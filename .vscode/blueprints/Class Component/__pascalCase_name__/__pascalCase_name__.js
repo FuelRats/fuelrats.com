94854dcd331d39108ca9a0e5d9cc9ea476a74477
Cameron Welter <camjwelt@gmail.com>
2020-05-20 00:53:50 +0000

    chore: replace old snippets with blueprint templates

---
+++ .vscode/blueprints/Class Component/__pascalCase_name__/__pascalCase_name__.js
@@ -0,0 +1,96 @@
+// Module imports
+import PropTypes from 'prop-types'
+import React from 'react'
+
+
+
+
+// Component imports
+import styles from './{{pascalCase name}}.module.scss'
+import { connect } from '~/store'
+
+
+
+
+// Component Constants
+
+
+
+
+
+@connect
+class {{pascalCase name}} extends React.Component {
+  /***************************************************************************\
+    Class Properties
+  \***************************************************************************/
+
+  state = {}
+
+
+
+
+
+  /***************************************************************************\
+    Private Methods
+  \***************************************************************************/
+
+
+
+
+
+  /***************************************************************************\
+    Public Methods
+  \***************************************************************************/
+
+  render () {
+    const {
+      className,
+    } = this.props
+
+    return (
+      <div className={[styles.{{camelCase name}}, className]} />
+    )
+  }
+
+
+
+
+
+  /***************************************************************************\
+    Getters
+  \***************************************************************************/
+
+
+
+
+
+  /***************************************************************************\
+    Redux Properties
+  \***************************************************************************/
+
+  static mapDispatchToProps = []
+
+  static mapStateToProps = () => {
+    return {}
+  }
+
+
+
+
+
+  /***************************************************************************\
+    Prop Definitions
+  \***************************************************************************/
+
+  static defaultProps = {}
+
+  static propTypes = {
+    className: PropTypes.string,
+  }
+}
+
+
+
+
+
+export default {{pascalCase name}}
