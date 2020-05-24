c4090789667fa22ebf8f07f31cafcaed5f06ac4c
Cameron Welter <camjwelt@gmail.com>
2020-05-24 08:55:27 +0000

    refactor(SilentBoundary): add note about component

--- src/components/SilentBoundary.js
+++ src/components/SilentBoundary.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 
 
-
+/**
+ * This is not intended to be a long-term solution. Eventually we will want to flesh this out with some form of error display.
+ * Currently we use this as a safeguard from the entire website failing out if a page crashes due to transition timing weirdness.
+ */
 class SilentBoundary extends React.Component {
   state = {
     hasError: false,
