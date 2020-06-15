41444d2ca93566277356110d9d1e20d67585e996
Cameron Welter <camjwelt@gmail.com>
2020-06-15 07:12:55 +0000

    chore: update templates to reflect new import order

--- .vscode/blueprints/Class Component/__pascalCase_name__/__pascalCase_name__.js
+++ .vscode/blueprints/Class Component/__pascalCase_name__/__pascalCase_name__.js
@@ -1,13 +1,9 @@
-// Module imports
 import PropTypes from 'prop-types'
 import React from 'react'
 
-
-
-
-// Component imports
-import styles from './{{pascalCase name}}.module.scss'
 import { connect } from '~/store'
+
+import styles from './{{pascalCase name}}.module.scss'
 
 
 
