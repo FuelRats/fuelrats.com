31541e0a803ff05f00376d38cf14e510502567c3
Cameron Welter <camjwelt@gmail.com>
2020-07-20 23:23:22 +0000

    refactor(useField): set default validity debounce wait to 500ms

--- src/hooks/useForm/useField.js
+++ src/hooks/useForm/useField.js
@@ -29,7 +29,7 @@ function useField (name = isRequired('name'), opts = {}) {
       setQueueState(false)
     },
     [validateField, name],
-    validateOpts ?? { wait: 250 },
+    validateOpts ?? { wait: 500 },
   )
 
   const queueValidation = useCallback((value, curState) => {
