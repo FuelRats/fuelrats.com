6bd08e53c2d0024d6b93276153bf3ff229096e1f
Cameron Welter <camjwelt@gmail.com>
2020-07-27 23:16:50 +0000

    fix(useValidityReducer-hook): properly merge fragments, recognize empty validity state as valid

--- src/hooks/useValidityReducer.js
+++ src/hooks/useValidityReducer.js
@@ -11,24 +11,28 @@ export default function useValidityReducer (initialState = {}) {
   const validityReducer = useCallback((draftState, action) => {
     const { name, valid, fragment } = action
     if (fragment) {
-      mergeReducer(draftState, fragment)
-    } else if (draftState[name] !== valid) {
+      return mergeReducer(draftState, fragment)
+    }
+
+    if (draftState[name] !== valid) {
       if (typeof valid === 'undefined') {
         delete draftState[name]
       } else {
         draftState[name] = valid
       }
     }
+
+    return undefined
   }, [])
 
   const [state = {}, dispatch] = useImmerReducer(validityReducer, initialState)
 
   const isValid = useMemo(() => {
     const validityValues = Object.values(state)
-    return validityValues.length && validityValues.filter((entry) => {
+    return Boolean(!validityValues.length || validityValues.filter((entry) => {
       // only "true" and "undefined" are considered valid values
       return entry !== true || typeof entry === 'undefined'
-    }).length === 0
+    }).length === 0)
   }, [state])
 
   return [isValid, dispatch]
