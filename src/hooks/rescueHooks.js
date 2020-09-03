62715471e8358584c3b369ae7e915be0d89290c6
Cameron Welter <camjwelt@gmail.com>
2020-09-03 07:06:19 +0000

    feat(rescueHooks-hooks): add memoization hooks for rescue data

---
+++ src/hooks/rescueHooks.js
@@ -0,0 +1,29 @@
+import { useMemo } from 'react'
+
+import { getLanguage } from '~/data/LanguageList'
+import { getPlatform } from '~/data/PlatformList'
+import { formatAsEliteDateTime } from '~/helpers/formatTime'
+
+export const useQuoteString = (rescue) => {
+  return useMemo(() => {
+    if (!rescue.attributes.quotes.length) {
+      return undefined
+    }
+
+    return rescue.attributes.quotes.reduce((acc, quote) => {
+      return `${acc}[${formatAsEliteDateTime(quote.createdAt)}] "${quote.message}" - ${quote.author}\n`
+    }, [])
+  }, [rescue?.attributes?.quotes])
+}
+
+export const useLanguageData = (rescue) => {
+  return useMemo(() => {
+    return getLanguage(rescue.attributes.clientLanguage)
+  }, [rescue.attributes.clientLanguage])
+}
+
+export const usePlatformData = (rescue) => {
+  return useMemo(() => {
+    return getPlatform(rescue.attributes.platform)
+  }, [rescue.attributes.platform])
+}
