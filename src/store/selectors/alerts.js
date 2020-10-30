520a9d8cf1ab61de08f317160dc1910e4d245874
Cameron Welter <camjwelt@gmail.com>
2020-10-30 16:24:26 +0000

    feat(alert-selectors): add selector for selecting active alerts + overflow count

--- src/store/selectors/alerts.js
+++ src/store/selectors/alerts.js
@@ -1,3 +1,15 @@
+import { createSelector } from 'reselect'
+
+const ACTIVE_ALERT_LIMIT = 5
+
+
 export const selectAlerts = (state) => {
   return state.alerts
 }
+
+export const selectActiveAlerts = createSelector(
+  [selectAlerts],
+  (alerts) => {
+    return [alerts.slice(0, ACTIVE_ALERT_LIMIT), Math.max(alerts.length - ACTIVE_ALERT_LIMIT, 0)]
+  },
+)
