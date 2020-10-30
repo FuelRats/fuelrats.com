6c7b1bfdc8cad881899dde151cdefebbb922fb19
Cameron Welter <camjwelt@gmail.com>
2020-10-30 16:24:26 +0000

    feat(alert-actions): add ID generator to createAlert

--- src/store/actions/alerts.js
+++ src/store/actions/alerts.js
@@ -1,4 +1,5 @@
 import { createFSA } from '@fuelrats/web-util/actions'
+import UUID from 'pure-uuid'
 
 import { presentJSONAPIResource } from '~/helpers/presenters'
 
@@ -10,7 +11,7 @@ import actionTypes from '../actionTypes'
 
 // Module constants
 const DEFAULT_TIMEOUT_LENGTH = 1200 // 12 seconds
-
+const UUID_VERSION_4 = 4
 
 
 
@@ -19,10 +20,11 @@ export function createAlert (data = {}) {
   return createFSA(
     actionTypes.alerts.create,
     presentJSONAPIResource('web-alerts', {
-      alertType: 'info', // 'error', 'warn', 'info'
+      alertType: 'info', // 'success', 'info', 'warn', 'error'
       timeout: DEFAULT_TIMEOUT_LENGTH,
       createdAt: (new Date()).toISOString(),
       ...data,
+      id: data.id ?? (new UUID(UUID_VERSION_4)).format(),
     }),
   )
 }
