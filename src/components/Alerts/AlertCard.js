f7fdc3d3ab6e3b63962f20e903c513cf9a35bf6b
Cameron Welter <camjwelt@gmail.com>
2020-10-30 16:24:26 +0000

    feat(AlertCard): add basic card structure

--- src/components/Alerts/AlertCard.js
+++ src/components/Alerts/AlertCard.js
@@ -1,5 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { motion } from 'framer-motion'
 import PropTypes from 'prop-types'
-import React from 'react'
+import { useCallback, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { deleteAlert } from '~/store/actions/alerts'
+
+import styles from './Alerts.module.scss'
+
+
+
+
+
+// Component constants
+const cardMotionConfig = {
+
+}
+
+const alertTypeIcons = {
+  success: 'thumbs-up',
+  info: 'info-circle',
+  warn: 'exclamation-circle',
+  error: 'exclamation-triangle',
+}
 
 
 
@@ -7,11 +30,62 @@ import React from 'react'
 
 function AlertCard (props) {
   const {
+    alert,
     className,
   } = props
 
+  const dispatch = useDispatch()
+
+  const handleDismiss = useCallback(() => {
+    dispatch(deleteAlert(alert))
+  }, [alert, dispatch])
+
+  useEffect(() => {
+    setTimeout(handleDismiss, alert.attributes.timeout)
+  })
+
+  const {
+    title,
+    body,
+    alertType,
+  } = alert.attributes
+
   return (
-    <div className={className} />
+    <motion.div key={alert.id} {...cardMotionConfig} className={className}>
+      <header className={styles.header}>
+        <h4>
+          <FontAwesomeIcon fixedWidth icon={alertTypeIcons[alertType]} />
+          {title}
+        </h4>
+        <button
+          className="compact"
+          type="button"
+          onClick={handleDismiss}>
+          <FontAwesomeIcon fixedWidth icon="times" />
+        </button>
+      </header>
+      <div className={styles.body}>
+        {body}
+      </div>
+      {
+        alertType === AlertCard.Type.ERROR && (
+          <menu type="toolbar">
+            <div className="secondary">
+              <a
+                className="button inline link"
+                href="https://jira.fuelrats.com/servicedesk/customer/portal/1"
+                rel="noopener noreferrer"
+                target="_blank">
+                <small>
+                  <FontAwesomeIcon fixedWidth icon="bug" />
+                  {' Report this error.'}
+                </small>
+              </a>
+            </div>
+          </menu>
+        )
+      }
+    </motion.div>
   )
 }
 
@@ -26,8 +100,8 @@ AlertCard.propTypes = {
   alert: PropTypes.shape({
     attributes: PropTypes.shape({
       alertType: PropTypes.oneOf(Object.values(AlertCard.Type)).isRequired,
+      body: PropTypes.node.isRequired,
       createdAt: PropTypes.string.isRequired,
-      message: PropTypes.string.isRequired,
       timeout: PropTypes.number.isRequired,
       title: PropTypes.string,
     }).isRequired,
