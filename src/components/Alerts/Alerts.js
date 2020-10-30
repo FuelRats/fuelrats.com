b661e3749df1cd9265784e2a4dd3d504178a381f
Cameron Welter <camjwelt@gmail.com>
2020-10-30 16:24:26 +0000

    feat(alerts): add animations for cards

--- src/components/Alerts/Alerts.js
+++ src/components/Alerts/Alerts.js
@@ -1,12 +1,31 @@
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion'
 import PropTypes from 'prop-types'
 import { createPortal } from 'react-dom'
 import { useSelector } from 'react-redux'
 
-import { selectAlerts } from '~/store/selectors/alerts'
+import { selectActiveAlerts } from '~/store/selectors/alerts'
 
 import AlertCard from './AlertCard'
 import styles from './Alerts.module.scss'
+
+
+
+
+// Component constants
+const cardMotionConfig = {
+  initial: { opacity: 0, x: 300 },
+  animate: { opacity: 1, x: 0 },
+  exit: {
+    opacity: 0,
+    height: 0,
+    pointerEvents: 'none',
+  },
+  transition: {
+    type: 'spring',
+    stiffness: 550,
+    damping: 50,
+  },
+}
 
 
 
@@ -17,21 +36,38 @@ function Alerts (props) {
     className,
   } = props
 
-  const activeAlerts = useSelector(selectAlerts)
+  const [activeAlerts, overflow] = useSelector(selectActiveAlerts)
+
+  if (typeof window === 'undefined') {
+    return null
+  }
 
   return createPortal(
     (
       <div className={[styles.alerts, className]}>
-        <AnimatePresence>
-          {
-            activeAlerts.map((alert) => {
-              return (<AlertCard key={alert.id} alert={alert} />)
-            })
-          }
-        </AnimatePresence>
+        <AnimateSharedLayout>
+          <AnimatePresence>
+            {
+              activeAlerts.map((alert) => {
+                return (
+                  <motion.div key={alert.id} layout {...cardMotionConfig}>
+                    <AlertCard alert={alert} />
+                  </motion.div>
+                )
+              })
+            }
+          </AnimatePresence>
+        </AnimateSharedLayout>
+        {
+          (overflow > 0) && (
+            <motion.span layout className={styles.overflow}>
+              {`+${overflow} more`}
+            </motion.span>
+          )
+        }
       </div>
     ),
-    document.getElementById('alert-container'),
+    document.getElementById('AlertContainer'),
   )
 }
 
