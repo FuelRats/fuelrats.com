bf3ac20014edc9dec92f6d9c2aefe323c59267df
Cameron Welter <camjwelt@gmail.com>
2020-10-30 16:24:26 +0000

    refactor(ScopeView): convert to function component, optimize scope reducer with reselect

--- src/components/ScopeView/ScopeView.js
+++ src/components/ScopeView/ScopeView.js
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types'
-import React from 'react'
-import { createStructuredSelector } from 'reselect'
+import { createSelector } from 'reselect'
 
-import { connect } from '~/store'
+import useSelectorWithProps from '~/hooks/useSelectorWithProps'
 import { selectCurrentUserScopes } from '~/store/selectors'
 
 import NamespaceDetails from './NamespaceDetails'
@@ -14,118 +13,64 @@ import styles from './ScopeView.module.scss'
 
 // Component Constants
 
-const groupScopes = (scopes, accessibleScopes = []) => {
-  return scopes.reduce((acc, scopeStr) => {
-    const [namespace, action, isSelf] = scopeStr.split('.')
+const selectAccessibleScopeData = createSelector(
+  [
+    (_, props) => {
+      return props.scopes
+    },
+    selectCurrentUserScopes,
+  ],
+  (requestedScopes, userScopes = []) => {
+    return requestedScopes.reduce((acc, scopeStr) => {
+      const [namespace, action, isSelf] = scopeStr.split('.')
 
-    if (!acc[namespace]) {
-      acc[namespace] = { [action]: {} }
-    } else if (!acc[namespace][action]) {
-      acc[namespace][action] = {}
-    }
+      if (!acc[namespace]) {
+        acc[namespace] = { [action]: {} }
+      } else if (!acc[namespace][action]) {
+        acc[namespace][action] = {}
+      }
 
-    acc[namespace][action][isSelf ? 'self' : 'all'] = {
-      accessible: accessibleScopes.includes(scopeStr)
-        || (isSelf && accessibleScopes.includes(`${namespace}.${action}`))
-        || accessibleScopes.includes('*'),
-    }
+      acc[namespace][action][isSelf ? 'self' : 'all'] = {
+        accessible: userScopes.includes(scopeStr)
+        || (isSelf && userScopes.includes(`${namespace}.${action}`))
+        || userScopes.includes('*'),
+      }
 
-    return acc
-  }, {})
+      return acc
+    }, {})
+  },
+)
+
+
+
+
+
+function ScopeView (props) {
+  const { className, scopes } = props
+
+  const groupedScopes = useSelectorWithProps({ scopes }, selectAccessibleScopeData)
+
+  return (
+    <div className={[styles.scopeView, className]}>
+      {
+        Object.entries(groupedScopes).map(
+          ([namespace, actions]) => {
+            return (
+              <NamespaceDetails
+                key={namespace}
+                actions={actions}
+                namespace={namespace} />
+            )
+          },
+        )
+      }
+    </div>
+  )
 }
 
-
-
-
-
-@connect
-class ScopeView extends React.PureComponent {
-  /***************************************************************************\
-    Class Properties
-  \***************************************************************************/
-
-  state = {}
-
-
-
-
-
-  /***************************************************************************\
-    Private Methods
-  \***************************************************************************/
-
-
-
-
-
-  /***************************************************************************\
-    Public Methods
-  \***************************************************************************/
-
-  render () {
-    const {
-      accessibleScopes,
-      className,
-      scopes,
-    } = this.props
-
-    const groupedScopes = groupScopes(scopes, accessibleScopes)
-
-    return (
-      <div className={[styles.scopeView, className]}>
-        {
-          Object.entries(groupedScopes).map(
-            ([namespace, actions]) => {
-              return (
-                <NamespaceDetails
-                  key={namespace}
-                  actions={actions}
-                  namespace={namespace} />
-              )
-            },
-          )
-        }
-      </div>
-    )
-  }
-
-
-
-
-
-  /***************************************************************************\
-    Getters
-  \***************************************************************************/
-
-
-
-
-
-  /***************************************************************************\
-    Redux Properties
-  \***************************************************************************/
-
-  static mapStateToProps = createStructuredSelector({
-    accessibleScopes: selectCurrentUserScopes,
-  })
-
-
-
-
-
-  /***************************************************************************\
-    Prop Definitions
-  \***************************************************************************/
-
-  static defaultProps = {
-    className: undefined,
-  }
-
-  static propTypes = {
-    accessibleScopes: PropTypes.arrayOf(PropTypes.string).isRequired,
-    className: PropTypes.string,
-    scopes: PropTypes.arrayOf(PropTypes.string).isRequired,
-  }
+ScopeView.propTypes = {
+  className: PropTypes.string,
+  scopes: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 
