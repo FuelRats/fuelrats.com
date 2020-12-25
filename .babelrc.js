3dc9def72d1725e59a64aca3558b0e563bb76596
Cameron Welter <camjwelt@gmail.com>
2020-12-25 12:43:14 +0000

    chore: convert .babelrc for pnp compatibility

---
+++ .babelrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "presets": [
+    [
+      require.resolve("next/babel"),
+      {
+        "preset-env": {
+          "bugfixes": true,
+          "targets": "defaults, not IE 11, not IE_mob 11, node 13"
+        },
+        "class-properties": {
+          "loose": true
+        }
+      }
+    ]
+  ],
+  "plugins": [
+    [require.resolve("@fuelrats/babel-plugin-classnames"), { "transformObjects": true, "packageName": "clsx" }],
+    [require.resolve("@babel/plugin-proposal-decorators"), { "legacy": true }],
+    require.resolve("@babel/plugin-proposal-export-default-from"),
+    require.resolve("@babel/plugin-proposal-export-namespace-from"),
+    require.resolve("@babel/plugin-proposal-optional-catch-binding")
+  ]
+}
