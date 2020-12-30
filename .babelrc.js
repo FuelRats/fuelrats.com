bc95b49126a092401507e1dcc12596accbb4a950
Cameron Welter <camjwelt@gmail.com>
2020-12-30 14:54:50 +0000

    style(babel-config): fix lint issues

--- .babelrc.js
+++ .babelrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-  "presets": [
+  presets: [
     [
-      require.resolve("next/babel"),
+      require.resolve('next/babel'),
       {
-        "preset-env": {
-          "bugfixes": true,
-          "targets": "defaults, not IE 11, not IE_mob 11, node 13"
+        'preset-env': {
+          bugfixes: true,
+          targets: 'defaults, not IE 11, not IE_mob 11, node 13',
         },
-        "class-properties": {
-          "loose": true
-        }
-      }
-    ]
+        'class-properties': {
+          loose: true,
+        },
+      },
+    ],
   ],
-  "plugins": [
-    [require.resolve("@fuelrats/babel-plugin-classnames"), { "transformObjects": true, "packageName": "clsx" }],
-    [require.resolve("@babel/plugin-proposal-decorators"), { "legacy": true }],
-    require.resolve("@babel/plugin-proposal-export-default-from"),
-    require.resolve("@babel/plugin-proposal-export-namespace-from"),
-    require.resolve("@babel/plugin-proposal-optional-catch-binding")
-  ]
+  plugins: [
+    [require.resolve('@fuelrats/babel-plugin-classnames'), { transformObjects: true, packageName: 'clsx' }],
+    [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+    require.resolve('@babel/plugin-proposal-export-default-from'),
+    require.resolve('@babel/plugin-proposal-export-namespace-from'),
+    require.resolve('@babel/plugin-proposal-optional-catch-binding'),
+  ],
 }
