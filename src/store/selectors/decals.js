432dc8bf8346da1b5e467ab8de79bc77ebc8d9a5
Cameron Welter <camjwelt@gmail.com>
2021-01-07 17:27:47 +0000

    feat(decal-selectors): add redeemable decals selector

--- src/store/selectors/decals.js
+++ src/store/selectors/decals.js
@@ -1,5 +1,8 @@
 import { createSelector } from 'reselect'
 
+import safeParseInt from '~/helpers/safeParseInt'
+
+import { withCurrentUserId } from './session'
 import { selectUserById } from './users'
 
 
@@ -17,6 +20,11 @@ export const selectDecalEligibility = (state) => {
 
 export const selectDecalById = (state, { decalId } = {}) => {
   return selectDecals(state)[decalId]
+}
+
+
+export const selectUserRedeemableDecals = (state, props) => {
+  return safeParseInt(withCurrentUserId(selectUserById)(state, props)?.meta?.redeemable, undefined, 0)
 }
 
 
