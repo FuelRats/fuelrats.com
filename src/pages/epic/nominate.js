96c32587129b5ffc45161b88eac806d46eb22bc9
Delota <smissot+delota@gmail.com>
2021-04-14 20:26:03 +0200

    You shall not pass!

--- src/pages/epic/nominate.js
+++ src/pages/epic/nominate.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import { authenticated } from '~/components/AppLayout'
 import EpicNominationForm from '~/components/Forms/EpicNominationForm/EpicNominationForm'
 
 
+@authenticated('epics.write.me', 'Sorry, you must be a rat to access the nomination page.')
 export default class EpicNominatePage extends React.Component {
   static getPageMeta () {
     return {
