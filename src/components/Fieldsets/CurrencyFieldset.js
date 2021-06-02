5e6d215a7e2e3d53e4762fe07390ce74a6d7c843
Cameron Welter <camjwelt@gmail.com>
2021-06-02 13:20:58 +0000

    feat(CurrencyFieldset): add fieldset for inputting currencies

---
+++ src/components/Fieldsets/CurrencyFieldset.js
@@ -0,0 +1,39 @@
+import { useCallback } from 'react'
+
+import InputFieldset from './InputFieldset'
+
+
+
+
+
+const currencyValidator = /^[0-9]{0,6}(\.[0-9]{0,2})?$/u
+
+
+
+
+
+export default function CurrencyFieldset (props) {
+  const {
+    onChange: parentChange,
+    ...inputProps
+  } = props
+
+  const onChange = useCallback(
+    (event, value) => {
+      if (!value.match(currencyValidator)) {
+        event.preventDefault()
+      }
+
+      parentChange(event, value)
+    },
+    [parentChange],
+  )
+
+  return (
+    <InputFieldset
+      placeholder="0.00"
+      {...inputProps}
+      type="text"
+      onChange={onChange} />
+  )
+}
