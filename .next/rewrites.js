7b87e99b274837fb166b169a9d213d391c4c4b12
Cameron Welter <camjwelt@gmail.com>
2021-06-30 07:32:27 +0000

    chore: separate redirects and rewrites so next config is easier to consume

---
+++ .next/rewrites.js
@@ -0,0 +1,25 @@
+module.exports = (env) => {
+  return () => {
+    return {
+      beforeFiles: [
+        {
+          // Apple demands this be on the site root, but all api routes must be in /api. oof.
+          source: '/apple-app-site-association',
+          destination: '/api/apple-app-site-association',
+        },
+        {
+          // Blog content dir.
+          source: '/wp-content/:path*',
+          destination: `${env.wordpress.url}/wp-content/:path*`,
+        },
+      ],
+      fallback: [
+        {
+          // Old blog links. yikes.
+          source: '/:year/:month/:day/:slug',
+          destination: '/blog/:slug',
+        },
+      ],
+    }
+  }
+}
--- next.config.js
+++ next.config.js
@@ -7,8 +7,9 @@ import path from 'path'
 import url from 'url'
 import { DefinePlugin } from 'webpack'
 
-import { getEnv } from './src/util/server'
-
+import redirects from './.next/redirects'
+import rewrites from './.next/rewrites'
+import getEnv from './src/util/server/getEnv'
 
 
 
@@ -75,98 +76,8 @@ const next = {
       rat: env.irc.rat,
     },
   },
-  rewrites: () => {
-    return {
-      beforeFiles: [
-        {
-          // Apple demands this be on the site root, but all api routes must be in /api. oof.
-          source: '/apple-app-site-association',
-          destination: '/api/apple-app-site-association',
-        },
-        {
-          // Blog content dir.
-          source: '/wp-content/:path*',
-          destination: `${env.wordpress.url}/wp-content/:path*`,
-        },
-      ],
-      fallback: [
-        {
-          // Old blog links. yikes.
-          source: '/:year/:month/:day/:slug',
-          destination: '/blog/:slug',
-        },
-      ],
-    }
-  },
-  redirects: () => {
-    return [
-      {
-        // All IE traffic should go to fallbackUrl.
-        source: '/:path*',
-        destination: env.fallbackUrl,
-        permanent: false,
-        basePath: false,
-        has: [
-          {
-            type: 'header',
-            key: 'User-Agent',
-            value: '(.*Trident.*)',
-          },
-        ],
-      },
-      {
-        // Temporary workaround so canceled donations return to donate screen
-        source: '/donate/cancel',
-        destination: '/donate',
-        permanent: false,
-      },
-      {
-        // Profile page requires a tab name in the path
-        source: '/profile',
-        destination: '/profile/overview',
-        permanent: true,
-      },
-      {
-        // Old blog used to exist at /blogs
-        source: '/blogs',
-        destination: '/blog',
-        permanent: true,
-      },
-      {
-        // get-help was used at launch of the website, but has since been changed back.
-        source: '/get-help',
-        destination: '/i-need-fuel',
-        permanent: true,
-      },
-      {
-        // Common endpoint for privacy policies
-        source: '/privacy',
-        destination: '/privacy-policy',
-        permanent: true,
-      },
-      {
-        // Lexicon is no longer local
-        source: '/fuel-rats-lexicon',
-        destination: 'https://confluence.fuelrats.com/pages/viewpage.action?pageId=3637257',
-        permanent: true,
-        basePath: false,
-      },
-      {
-        // statistics are no longer local
-        source: '/statistics',
-        destination: 'https://grafana.fuelrats.com',
-        permanent: true,
-        basePath: false,
-      },
-      {
-        // People often type this one manually into their URL bar to get to the helpdesk
-        source: '/help',
-        destination: 'https://t.fuelr.at/help',
-        permanent: true,
-        basePath: false,
-      },
-    ]
-  },
+  redirects: redirects(env),
+  rewrites: rewrites(env),
   webpack: (config, opt) => {
     /* Define Plugin */
     config.plugins.push(new DefinePlugin({
