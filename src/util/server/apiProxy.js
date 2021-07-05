26d8ee4570b12896a11b9a213fd9bffb3edf93b8
Cameron Welter <camjwelt@gmail.com>
2021-07-05 01:01:31 +0000

    refactor(apiProxy-util): configure logger

--- src/util/server/apiProxy.js
+++ src/util/server/apiProxy.js
@@ -1,7 +1,17 @@
 import { createProxyMiddleware } from 'http-proxy-middleware'
+import * as log from 'next/dist/build/output/log'
 
 
 
+function logProvider () {
+  return {
+    log: log.info,
+    debug: log.info,
+    info: log.info,
+    warn: log.warn,
+    error: log.error,
+  }
+}
 
 
 export const config = {
@@ -14,6 +24,8 @@ export const config = {
 export default function apiProxy (options) {
   return createProxyMiddleware({
     changeOrigin: true,
+    logLevel: $$BUILD.isDev ? 'debug' : 'warn',
+    logProvider,
     secure: true,
     xfwd: true,
     ...options,
