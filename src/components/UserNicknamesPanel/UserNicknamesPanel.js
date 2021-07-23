c322f859fd3a3d0f65df578ddc161844249bacde
Cameron Welter <camjwelt@gmail.com>
2021-07-23 19:27:45 +0000

    ♻️ (UserNicknamesPanel): rewrite to hooks-based function component

--- src/components/UserNicknamesPanel/UserNicknamesPanel.js
+++ src/components/UserNicknamesPanel/UserNicknamesPanel.js
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { HttpStatus } from '@fuelrats/web-util/http'
 import { isError } from 'flux-standard-action'
-import React from 'react'
+import { useCallback, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { connect } from '~/store'
+import { deleteNickname } from '~/store/actions/user'
 import {
   selectUserById,
   withCurrentUserId,
@@ -25,36 +26,17 @@ const MAX_NICKS = 16 // Maximum IRC Nicknames allowed
 
 
 
+function UserNicknamesPanel () {
+  const [error, setError] = useState(null)
 
-@connect
-class UserNicknamesPanel extends React.Component {
-  /***************************************************************************\
-    Class Properties
-  \***************************************************************************/
+  const dispatch = useDispatch()
+  const nicknames = useSelector(withCurrentUserId(selectNicknamesByUserId))
+  const user = useSelector(withCurrentUserId(selectUserById))
 
-  state = {
-    error: null,
-    formOpen: false,
-  }
-
-
-
-
-
-  /***************************************************************************\
-    Private Methods
-  \***************************************************************************/
-
-  _handleFormVisibilityToggle = () => {
-    this.setState((state) => {
-      return { formOpen: !state.formOpen }
-    })
-  }
-
-  _handleDeleteNickname = async (event) => {
-    const response = await this.props.deleteNickname(this.props.user, this.props.nicknames.find((nick) => {
+  const handleDeleteNickname = useCallback(async (event) => {
+    const response = await dispatch(deleteNickname(user, nicknames.find((nick) => {
       return nick.id === event.target.name
-    }))
+    })))
 
     if (isError(response)) {
       const { meta, payload } = response
@@ -68,102 +50,71 @@ class UserNicknamesPanel extends React.Component {
         errorMessage = 'Server communication error'
       }
 
-      this.setState({
-        error: errorMessage,
-      })
+      setError(errorMessage)
       return errorMessage
     }
 
     return undefined
-  }
+  }, [dispatch, nicknames, user])
 
+  const nickCount = nicknames?.length
+  const maxNicksReached = (nickCount >= MAX_NICKS)
 
+  return (
+    <div className="panel">
+      <header>
+        {'IRC Nicknames'}
+        <div className="controls">
+          <span className="nickname-count">{`${nickCount}/${MAX_NICKS}`}</span>
+        </div>
+      </header>
 
-
-
-  /***************************************************************************\
-    Public Methods
-  \***************************************************************************/
-
-  render () {
-    const {
-      nicknames,
-    } = this.props
-
-    const { error } = this.state
-
-    const nickCount = nicknames?.length
-    const maxNicksReached = (nickCount >= MAX_NICKS)
-
-    return (
-      <div className="panel">
-        <header>
-          {'IRC Nicknames'}
-
-          <div className="controls">
-            <span className="nickname-count">{`${nickCount}/${MAX_NICKS}`}</span>
-          </div>
-        </header>
-
-        <div className={styles.userNicknames}>
+      <div className={styles.userNicknames}>
+        {
+          error && (
+            <MessageBox>{error}</MessageBox>
+          )
+        }
+        <ul>
           {
-            error && (
-              <MessageBox>{error}</MessageBox>
+            (nickCount <= 0) && (
+              <li className="text-center">{'You do not have any nicknames registered yet.'}</li>
             )
           }
-          <ul>
-            {(nickCount <= 0) && (<li className="text-center">{'You do not have any nicknames registered yet.'}</li>)}
-            {
-              nicknames?.map((nickname) => {
-                return (
-                  <li key={nickname.id}>
-                    <span>{nickname.attributes?.nick}</span>
-                    {
-                      // Only render for additional nicks, prevent for display nick.
-                      nickname.attributes?.display !== nickname.attributes?.nick && (
-                        <ConfirmActionButton
-                          className="icon"
-                          confirmButtonText={`Delete nickname '${nickname.attributes?.nick}'`}
-                          confirmSubText=""
-                          denyButtonText="Cancel"
-                          name={nickname.id}
-                          onConfirm={this._handleDeleteNickname}
-                          onConfirmText="">
-                          <FontAwesomeIcon fixedWidth icon="trash" />
-                        </ConfirmActionButton>
-                      )
-                    }
-                  </li>
-                )
-              }) ?? null
-            }
-          </ul>
+          {
+            nicknames?.map((nickname) => {
+              return (
+                <li key={nickname.id}>
+                  <span>{nickname.attributes?.nick}</span>
+                  {
+                    // Only render for additional nicks, prevent for display nick.
+                    nickname.attributes?.display !== nickname.attributes?.nick && (
+                      <ConfirmActionButton
+                        className="icon"
+                        confirmButtonText={`Delete nickname '${nickname.attributes?.nick}'`}
+                        confirmSubText=""
+                        denyButtonText="Cancel"
+                        name={nickname.id}
+                        onConfirm={handleDeleteNickname}
+                        onConfirmText="">
+                        <FontAwesomeIcon fixedWidth icon="trash" />
+                      </ConfirmActionButton>
+                    )
+                  }
+                </li>
+              )
+            }) ?? null
+          }
+        </ul>
 
-          <div className={styles.addNicknameFloat}>
-            <AddNicknameForm
-              disabled={maxNicksReached}
-              title={maxNicksReached ? 'You\'ve used all your nicknames' : 'Add new nickname'} />
-          </div>
+        <div className={styles.addNicknameFloat}>
+          <AddNicknameForm
+            disabled={maxNicksReached}
+            title={maxNicksReached ? 'You\'ve used all your nicknames' : 'Add new nickname'} />
         </div>
       </div>
-    )
-  }
-
-
-
-
-  /***************************************************************************\
-    Redux Properties
-  \***************************************************************************/
-
-  static mapDispatchToProps = ['deleteNickname']
-
-  static mapStateToProps = (state) => {
-    return {
-      nicknames: withCurrentUserId(selectNicknamesByUserId)(state),
-      user: withCurrentUserId(selectUserById)(state),
-    }
-  }
+    </div>
+  )
 }
 
 
