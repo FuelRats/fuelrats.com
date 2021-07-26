7fb0674f783397493c742c4eb39e6bb6bdc12be7
Cameron Welter <camjwelt@gmail.com>
2021-07-26 05:00:25 +0000

    ✨ (WordpressPage): add client-side page loading logic to WordpressPage

--- src/components/WordpressPage.js
+++ src/components/WordpressPage.js
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
+import { useDispatch } from 'react-redux'
 
 import useSelectorWithProps from '~/hooks/useSelectorWithProps'
+import useToggleState from '~/hooks/useToggleState'
+import { getWordpressPage } from '~/store/actions/wordpress'
 import { selectWordpressPageBySlug } from '~/store/selectors'
 
 
@@ -15,26 +18,37 @@ import { selectWordpressPageBySlug } from '~/store/selectors'
 
 
 function WordpressPage ({ className, slug }) {
-  const { content } = useSelectorWithProps({ slug }, selectWordpressPageBySlug) ?? {}
+  const page = useSelectorWithProps({ slug }, selectWordpressPageBySlug)
+  const [loading, toggleLoading] = useToggleState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const checkPageExistance = async () => {
+      if (!page) {
+        toggleLoading(true)
+        await dispatch(getWordpressPage(slug))
+      }
+      toggleLoading(false)
+    }
+    checkPageExistance()
+  // eslint-disable-next-line react-hooks/exhaustive-deps -- only run on slug change.
+  }, [slug])
 
   const renderedContent = useMemo(() => {
-    if (!content?.rendered) {
-      return null
+    if (!page?.content?.rendered) {
+      return undefined
     }
 
-    return content.rendered
+    return page.content.rendered
       .replace(/<ul>/giu, '<ul class="bulleted">')
       .replace(/<ol>/giu, '<ol class="numbered">')
-  }, [content?.rendered])
-
-  if (!renderedContent) {
-    return null
-  }
+  }, [page?.content?.rendered])
 
   /* eslint-disable react/no-danger */
   return (
     <div
-      className={['article-content', className]}
+      className={['article-content', { error: !renderedContent && !loading }, className]}
       dangerouslySetInnerHTML={{ __html: renderedContent }} />
   )
   /* eslint-enable */
