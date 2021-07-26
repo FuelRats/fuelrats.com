de2822a9c5f4063c7bcf139f71a94f63e082b613
Cameron Welter <camjwelt@gmail.com>
2021-07-26 05:02:00 +0000

    ♻️ (wordpress-proxy-page): rewrite to use <WordpressPage /> and hooks

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -1,46 +1,17 @@
 import { isError } from 'flux-standard-action'
-import { useMemo } from 'react'
 
-import useSelectorWithProps from '~/hooks/useSelectorWithProps'
+import WordpressPage from '~/components/WordpressPage'
 import { getWordpressPage } from '~/store/actions/wordpress'
 import { selectWordpressPageBySlug } from '~/store/selectors'
 import setError from '~/util/getInitialProps/setError'
 
-
-const selectPage = (state, props) => {
-  return selectWordpressPageBySlug(state, { slug: props.query?.slug }) ?? props.query?.page ?? {}
-}
-
-
 function WordpressProxy (props) {
-  const { content } = useSelectorWithProps(props, selectPage)
-
-  const renderedContent = useMemo(() => {
-    if (!content?.rendered) {
-      return null
-    }
-
-    return content.rendered.replace(/<ul>/giu, '<ul class="bulleted">').replace(/<ol>/giu, '<ol class="numbered">')
-  }, [content?.rendered])
-
-  if (!renderedContent) {
-    return (
-      <article className="error page-content" />
-    )
-  }
-
-  /* eslint-disable react/no-danger */
   return (
     <article className="page-content">
-      <div
-        className="article-content"
-        dangerouslySetInnerHTML={{ __html: renderedContent }} />
+      <WordpressPage slug={props.query?.slug} />
     </article>
   )
-  /* eslint-enable */
 }
-
-
 
 WordpressProxy.getInitialProps = async (ctx) => {
   const { query, store } = ctx
