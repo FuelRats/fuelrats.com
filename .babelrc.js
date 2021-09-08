78fbf786b2b605b31c7db5406fc781345c238f54
Cameron Welter <camjwelt@gmail.com>
2021-09-08 04:17:08 +0000

    🚨 fix babel loose mode warning

--- .babelrc.js
+++ .babelrc.js
@@ -7,15 +7,14 @@ module.exports = {
           bugfixes: true,
           targets: 'defaults, not IE 11, not IE_mob 11, node 16',
         },
-        'class-properties': {
-          loose: true,
-        },
       },
     ],
   ],
+  assumptions: {
+    setPublicClassFields: true,
+  },
   plugins: [
     [require.resolve('@fuelrats/babel-plugin-classnames'), { transformObjects: true, packageName: 'clsx' }],
     [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-    require.resolve('@babel/plugin-proposal-export-default-from'),
   ],
 }
