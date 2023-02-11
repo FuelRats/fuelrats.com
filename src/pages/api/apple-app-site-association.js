23bd0a52e9d8553783064e81184e480228a30f05
Alex S. Glomsaas <alex@sorlie.io>
2023-02-11 17:44:26 +0100

    Add support for IOS deeplinking

--- src/pages/api/apple-app-site-association.js
+++ src/pages/api/apple-app-site-association.js
@@ -5,6 +5,15 @@ import { HttpStatus } from '@fuelrats/web-util/http'
  */
 export default function appleAppSiteAssociation (req, res) {
   res.status(HttpStatus.OK).json({
+    appLinks: {
+      details: [
+        appIDs: ['9X89US848J.io.sorlie.Fuel-Rats'],
+        components: [{
+          '/': '/verify*',
+          'comment': 'used for handling password reset links in emails',
+        }],
+      ],
+    },
     webcredentials: {
       apps: ['9X89US848J.io.sorlie.Fuel-Rats'],
     },
