862758c5c7ff71ce9dd349d5c947b34756dfa9c1
Peter253545 <58088721+Peter253545@users.noreply.github.com>
2023-06-20 10:24:57 +0100

    Fix format of apple-app-site-association.

--- src/pages/api/apple-app-site-association.js
+++ src/pages/api/apple-app-site-association.js
@@ -1,18 +1,18 @@
 import { HttpStatus } from '@fuelrats/web-util/http'
 
 /*
- * Almost always redirected from '/apple-app-site-Association'
+ * Almost always rewritten from '/.well-known/apple-app-site-Association'
  */
 export default function appleAppSiteAssociation (req, res) {
   res.status(HttpStatus.OK).json({
     appLinks: {
-      details: [
+      details: [{
         appIDs: ['9X89US848J.io.sorlie.Fuel-Rats'],
         components: [{
           '/': '/verify*',
-          'comment': 'used for handling password reset links in emails',
+          comment: 'used for handling password reset links in emails',
         }],
-      ],
+      }],
     },
     webcredentials: {
       apps: ['9X89US848J.io.sorlie.Fuel-Rats'],
